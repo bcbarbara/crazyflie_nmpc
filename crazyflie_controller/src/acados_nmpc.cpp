65fb29c0ec663760727e6c3eff708f3d7b34f97e
bbarroscarlos <barros@diag.uniroma1.it>
2019-06-26 17:42:56 +0200

    joystick logitec map for teleop

--- crazyflie_controller/src/acados_nmpc.cpp
+++ crazyflie_controller/src/acados_nmpc.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
 #include <std_srvs/Empty.h>
+#include <std_msgs/String.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Vector3.h>
 #include <sensor_msgs/Joy.h>
@@ -39,6 +40,10 @@ ocp_nlp_dims * nlp_dims;
 
 external_function_casadi * forw_vde_casadi;
 
+
+using std::cout;
+using std::endl;
+
 // acados dim defines
 #define N 	40 	/* Number of intervals in the horizon. */
 #define NX 	6 	/* Number of differential state variables.  */
@@ -66,15 +71,13 @@ public:
         const ros::NodeHandle& n)
         : m_worldFrame(worldFrame)
         , m_frame(frame)
+	, m_state(Idle)
         , m_pubNav()
-        , m_listener()
-        , m_thrust(0)
 	, actual_x(0)
 	, actual_y(0)
 	, actual_z(0)
-	, goal_x(0)
-	, goal_y(0)
-	, goal_z(0)
+	, m_thrust(0)
+	, m_startZ(0)
     {
 	int status = 0;
 	status = acados_create();
@@ -85,13 +88,12 @@ public:
 	}
     
         ros::NodeHandle nh;
-        m_listener.waitForTransform(m_worldFrame, m_frame, ros::Time(0), ros::Duration(10.0)); 
-        m_pubNav = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
-	m_joy_sub = nh.subscribe<sensor_msgs::Joy>("joy", 10, &NMPC::joyCallback, this);
-	m_imu_sub = nh.subscribe("/crazyflie/imu", 1000, &NMPC::imuCallback, this);
-	m_viconpos_sub = nh.subscribe("/vicon/pos", 1000, &NMPC::posViconCallback, this);
-	m_service_takeoff = nh.advertiseService("takeoff", &NMPC::takeoff, this);
-        m_service_land = nh.advertiseService("land", &NMPC::land, this);
+
+        m_pubNav 	= nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+	m_joy_sub 	= nh.subscribe<sensor_msgs::Joy>("joy", 10, &NMPC::joyCallback, this);
+	//m_task_sub 	= nh.subscribe<>("task", 1000, &NMPC::taskCallback, this);
+	m_imu_sub 	= nh.subscribe("/crazyflie/imu", 1000, &NMPC::imuCallback, this);
+	m_viconpos_sub 	= nh.subscribe("/vicon/pos", 1000, &NMPC::posViconCallback, this);
     }
     
     void run(double frequency)
@@ -140,30 +142,6 @@ private:
     
     int acados_status;  
     
-    bool takeoff(
-        std_srvs::Empty::Request& req,
-        std_srvs::Empty::Response& res){
-       
-        ROS_INFO("Takeoff requested!");
-        m_state = TakingOff;
-
-        tf::StampedTransform transform;
-        m_listener.lookupTransform(m_worldFrame, m_frame, ros::Time(0), transform);
-        m_startZ = transform.getOrigin().z();
-
-        return true;
-    }
-
-    bool land(
-        std_srvs::Empty::Request& req,
-        std_srvs::Empty::Response& res){
-      
-        ROS_INFO("Landing requested!");
-        m_state = Landing;
-
-        return true;
-    }
-    
     void joyCallback(const sensor_msgs::Joy::ConstPtr& joy){  
       //Routine to manage joystick buttons as inputs
       int i;
@@ -177,9 +155,9 @@ private:
 	for(i=0;i<14;i++){
 		      last_hover_state[i]=m_hover[i];
 		      m_hover[i]=joy->buttons[i];
-		      std::cout << m_hover[i] << " " << last_hover_state[i]<< std::endl;
+		      cout << m_hover[i] << " " << last_hover_state[i]<< endl;
 	      }
-	      std::cout << " joy callback. " << std::endl;
+	      cout << " joy callback. " << endl;
 	      last_m_state = joy_state;
 	      for(i=0;i<14;i++){
 		      if (last_hover_state[i]!=m_hover[i]){
@@ -191,6 +169,19 @@ private:
       num++;
     }
     
+    /*void taskCallback( const std_msgs::String::ConstPtr& msg){
+      
+      const char* req;
+      req = msg->data.c_str();
+      
+      if(req == "idle") m_state = Idle;
+      if(req == "track") m_state = Tracking;
+      if(req == "takeoff") m_state = TakingOff;
+      if(req == "land") m_state = Landing;
+       
+      ROS_INFO(" Requesting: [%s]", req);
+    }*/
+    
     void posViconCallback( const geometry_msgs::Vector3::ConstPtr& msg){
       
 	actual_x = msg->x;
@@ -200,20 +191,11 @@ private:
     
     void imuCallback(const sensor_msgs::Imu::ConstPtr& msg){
       
-	actual_roll = msg->angular_velocity.x*3.141592654/180;
-	actual_pitch = msg->angular_velocity.y*3.141592654/180;
-	actual_yaw = msg->angular_velocity.z*3.141592654/180;
+	actual_roll  = msg->angular_velocity.x;
+	actual_pitch = msg->angular_velocity.y;
+	actual_yaw   = msg->angular_velocity.z;
     }
     
-    
-    void getTransform(
-        const std::string& sourceFrame,
-        const std::string& targetFrame,
-        tf::StampedTransform& result)
-    {
-        m_listener.lookupTransform(sourceFrame, targetFrame, ros::Time(0), result);
-    }
-
     void nmpcReset()
     {
         acados_free();
@@ -241,9 +223,8 @@ private:
 	      case TakingOff:
 		{
 		    ROS_INFO("Taking off.");
-		    tf::StampedTransform transform;
-		    m_listener.lookupTransform(m_worldFrame, m_frame, ros::Time(0), transform);
-		    if (transform.getOrigin().z() > m_startZ + 0.05 || m_thrust > 50000)
+
+		    if (actual_z > m_startZ + 0.05 || m_thrust > 50000)
 		    {
 			m_state = Tracking;
 			m_thrust = 0;
@@ -265,50 +246,38 @@ private:
 		    ROS_INFO("Landing.");
 		    geometry_msgs::Twist msg;
 		    
-		    tf::StampedTransform transform;
-		    m_listener.lookupTransform(m_worldFrame, m_frame, ros::Time(0), transform);
-		    if (transform.getOrigin().z() <= m_startZ + 0.05) {
+		    if (actual_z <= m_startZ + 0.05) {
 			m_state = Idle;
 			geometry_msgs::Twist msg;
 			m_pubNav.publish(msg);
-		    }
-		}
+		    } 
+		} // intentional fall thru
 		break;
+		
 		
 	      case Tracking:
 		{
 		    ROS_INFO("NMPC tracker.");
-		    tf::StampedTransform transform;
-		    m_listener.lookupTransform(m_worldFrame, m_frame, ros::Time(0), transform);
-
-		    geometry_msgs::PoseStamped targetWorld;
-		    targetWorld.header.stamp = transform.stamp_;
-		    targetWorld.header.frame_id = m_worldFrame;
-		    targetWorld.pose = m_goal.pose;
-
-		    geometry_msgs::PoseStamped targetDrone;
-		    m_listener.transformPose(m_frame, targetWorld, targetDrone);
-
-		    tfScalar roll, pitch, yaw;
-		    tf::Matrix3x3(
-			tf::Quaternion(
-			    targetDrone.pose.orientation.x,
-			    targetDrone.pose.orientation.y,
-			    targetDrone.pose.orientation.z,
-			    targetDrone.pose.orientation.w
-			)).getRPY(roll, pitch, yaw);
 
 		    geometry_msgs::Twist msg;
 		    solver_input acados_in;
 		    solver_output acados_out;
 		    
-		    // get position and velocity from mocap
+		    // get position and velocity from the mocap
 		    x0_sign[X] = actual_x;
 		    x0_sign[Y] = actual_y;
 		    x0_sign[Z] = actual_z;
 		    x0_sign[DX] = actual_x/dt;
 		    x0_sign[DY] = actual_y/dt;
 		    x0_sign[DZ] = actual_z/dt;
+		    
+		    // update reference
+		    yref_sign[0] = 0.0;
+		    yref_sign[1] = 0.0;
+		    yref_sign[2] = 0.4; // hovering at 40 cm high
+		    yref_sign[3] = 0.0;
+		    yref_sign[4] = 0.0;
+		    yref_sign[5] = 0.0;
 		    		    
 		    // copy signals into local buffers
 		    for (unsigned int i = 0; i < NX; i++) acados_in.x0[i] = x0_sign[i];
@@ -339,6 +308,7 @@ private:
 		    acados_out.KKT_res = (double)nlp_out->inf_norm_res;
 		    acados_out.cpu_time = (double)nlp_out->total_time;
 		    
+		    
 		    // get solution
 		    ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, 0, "u", (void *)acados_out.u0);
 
@@ -350,9 +320,12 @@ private:
 		    msg.linear.z = acados_out.u0[cf_thrust]; 	// thrust
 		    msg.angular.z = acados_out.u0[cf_yawr]; 	// yaw rate
 		    m_pubNav.publish(msg);
-		}
-		
-
+		    
+		    ROS_INFO_STREAM("thrust: " << acados_out.u0[cf_thrust] << " " << 
+				    "roll: "   << acados_out.u0[cf_roll] << " " << 
+				    "pitch: "  << acados_out.u0[cf_pitch] << " " <<
+				    "yaw rate: " << acados_out.u0[cf_yawr] << endl);
+		}		
 	}
     }
 
@@ -360,14 +333,14 @@ private:
   
     std::string m_worldFrame;
     std::string m_frame;
-    geometry_msgs::PoseStamped m_goal;
+
     ros::Publisher m_pubNav;
-    tf::TransformListener m_listener;
     ros::Subscriber m_joy_sub;
+    ros::Subscriber m_task_sub;
     ros::Subscriber m_imu_sub;
     ros::Subscriber m_viconpos_sub;
-     ros::ServiceServer m_service_takeoff;
-    ros::ServiceServer m_service_land;
+
+    // Flag to choose the task to perform
     task m_state;
     
     //Variables for joystick controller management
@@ -377,18 +350,20 @@ private:
     int last_m_state;
     double num;
     
-    //Variables of the control process
+    // Variables of the nmpc control process
     float x0_sign[NX];
     float yref_sign[NY];
-    float goal_x;
-    float goal_y;
-    float goal_z;
 
+    // Variables for take-off and landing services
     float m_thrust;
     float m_startZ;
+    
+    // Variables for the positions coming from the vicon
     float actual_x;
     float actual_y;
     float actual_z;
+    
+    // Variables for reading the IMU data
     float actual_roll;
     float actual_pitch;
     float actual_yaw;
@@ -402,12 +377,12 @@ int main(int argc, char **argv)
   ros::NodeHandle n("~");
   std::string worldFrame;
   n.param<std::string>("worldFrame", worldFrame, "/world");
-  std::string frame;
-  n.getParam("frame", frame);
+  std::string crazyflieFrame;
+  n.getParam("frame", crazyflieFrame);
   double frequency;
   n.param("frequency", frequency, 50.0);
 
-  NMPC nmpc(worldFrame, frame, n);
+  NMPC nmpc(worldFrame, crazyflieFrame, n);
   nmpc.run(frequency);
 
   return 0;
