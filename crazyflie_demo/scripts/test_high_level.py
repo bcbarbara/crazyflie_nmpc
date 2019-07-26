f6947e5d8c49c42d4235dfe6b66b75e5c611e579
bbarroscarlos <barros@diag.uniroma1.it>
2019-07-26 16:19:15 +0200

    HL commander launch

--- crazyflie_demo/scripts/test_high_level.py
+++ crazyflie_demo/scripts/test_high_level.py
@@ -8,41 +8,40 @@ import uav_trajectory
 if __name__ == '__main__':
     rospy.init_node('test_high_level')
 
-    cf = crazyflie.Crazyflie("crazyflie", "/vicon/crazyflie/crazyflie")
+    cf = crazyflie.Crazyflie("/crazyflie", "/mocap")
 
     cf.setParam("commander/enHighLevel", 1)
     cf.setParam("stabilizer/estimator", 2) # Use EKF
     cf.setParam("stabilizer/controller", 2) # Use mellinger controller
-
     # reset kalman
     cf.setParam("kalman/resetEstimation", 1)
 
-    # cf.takeoff(targetHeight = 0.5, duration = 2.0)
-    # time.sleep(3.0)
+    cf.takeoff(targetHeight = 0.5, duration = 2.0)
+    time.sleep(3.0)
 
-    # cf.goTo(goal = [0.5, 0.0, 0.0], yaw=0.2, duration = 2.0, relative = True)
-    # time.sleep(3.0)
+    cf.goTo(goal = [0.3, 0.0, 0.0], yaw=0.2, duration = 2.0, relative = True)
+    time.sleep(4.0)
 
-    # cf.land(targetHeight = 0.0, duration = 2.0)
+    cf.land(targetHeight = 0.0, duration = 2.0)
+    time.sleep(3.0)
+    #traj1 = uav_trajectory.Trajectory()
+    #traj1.loadcsv("/home/barbara/catkin_ws/src/crazyflie_ros/crazyflie_demo/scripts/takeoff.csv")
 
-    traj1 = uav_trajectory.Trajectory()
-    traj1.loadcsv("takeoff.csv")
+    #traj2 = uav_trajectory.Trajectory()
+    #traj2.loadcsv("/home/barbara/catkin_ws/src/crazyflie_ros/crazyflie_demo/scripts/figure8.csv")
 
-    traj2 = uav_trajectory.Trajectory()
-    traj2.loadcsv("figure8.csv")
+    #print(traj1.duration)
 
-    print(traj1.duration)
+    #cf.uploadTrajectory(0, 0, traj1)
+    #cf.uploadTrajectory(1, len(traj1.polynomials), traj2)
 
-    cf.uploadTrajectory(0, 0, traj1)
-    cf.uploadTrajectory(1, len(traj1.polynomials), traj2)
+    #cf.startTrajectory(0, timescale=1.0)
+    #time.sleep(traj1.duration * 2.0)
 
-    cf.startTrajectory(0, timescale=1.0)
-    time.sleep(traj1.duration * 2.0)
+    #cf.startTrajectory(1, timescale=2.0)
+    #time.sleep(traj2.duration * 2.0)
 
-    cf.startTrajectory(1, timescale=2.0)
-    time.sleep(traj2.duration * 2.0)
-
-    cf.startTrajectory(0, timescale=1.0, reverse=True)
-    time.sleep(traj1.duration * 1.0)
+    #cf.startTrajectory(0, timescale=1.0, reverse=True)
+    #time.sleep(traj1.duration * 1.0)
 
     cf.stop()
