ef8c0ad52328b520d9d99ae8920a25fe3e63dad9
bbarroscarlos <barros@diag.uniroma1.it>
2019-08-14 17:13:02 +0200

    acados_debug with logs

--- crazyflie_controller/src/acados_debug.cpp
+++ crazyflie_controller/src/acados_debug.cpp
@@ -23,6 +23,9 @@
 
 // standard
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <ios>
 
 // acados
 #include "acados/utils/print.h"
@@ -51,6 +54,7 @@ ocp_nlp_dims * nlp_dims;
 external_function_casadi * forw_vde_casadi;
 
 using namespace Eigen;
+using std::ofstream;
 using std::cout;
 using std::endl;
 using std::fixed;
@@ -68,7 +72,6 @@ using std::showpos;
 #define g0	9.80665
 #define WEIGHT_MATRICES 1
 #define REGULATION_POINT 1
-
 
 class NMPC
 {
@@ -130,6 +133,8 @@ public:
 
 	// Set elapsed time to zero initially
 	t0 = 0.0;
+	
+	LOG_ONCE = 1;
 	
 	// Steady-state control input value
 	mq = 33e-3; 	  		// [Kg]
@@ -238,6 +243,7 @@ public:
       double u0[NU];
       double x1[NX];
       double xN[NX];
+      double xAllStages[NX];
     };
 
     struct solver_input{
@@ -263,8 +269,8 @@ public:
 	double dq = 0;
 // 	if (elapsed_time > 0.01) dq = 1.573*dq_samples[4] - 0.6188*dq_samples[3] + 22.65*q_samples[4] - 22.65*q_samples[3];
 // 	else dq = (q_samples[4] - q_samples[3]) / Ts;
-	if (elapsed_time > 0.1) dq = 1.573*dq_samples[4] - 0.6188*dq_samples[3] + 22.65*q_samples[4] - 22.65*q_samples[3];
-	else dq = 0.0;
+	if (elapsed_time > 0.01) dq = 1.573*dq_samples[4] - 0.6188*dq_samples[3] + 22.65*q_samples[4] - 22.65*q_samples[3];
+	else dq = (q_samples[4] - q_samples[3]) / Ts;
 
 	return dq;
     }
@@ -457,180 +463,334 @@ public:
 
     void iteration(const ros::TimerEvent& e){
 
-#if 0
-
-#endif
-#if 1
       if(e.last_real.isZero()) {
-		t0 = e.current_real.toSec();
+	t0 = e.current_real.toSec();
+	//return;
       }
-
-      double dt = e.current_real.toSec() - e.last_real.toSec();
+	
+      double dt = e.current_real.toSec() - e.last_real.toSec();    
       
-      // Update reference
-      for (k = 0; k < N+1; k++) {
-	    yref_sign[k * NY + 0] = xq_des; 	// xq
-	    yref_sign[k * NY + 1] = yq_des;	// yq
-	    yref_sign[k * NY + 2] = zq_des;	// zq
-	    yref_sign[k * NY + 3] = 1.0;	// q1
-	    yref_sign[k * NY + 4] = 0.0;	// q2
-	    yref_sign[k * NY + 5] = 0.0;	// q3
-	    yref_sign[k * NY + 6] = 0.0;	// q4
-	    yref_sign[k * NY + 7] = 0.0;	// vbx
-	    yref_sign[k * NY + 8] = 0.0;	// vby
-	    yref_sign[k * NY + 9] = 0.0;	// vbz
-	    yref_sign[k * NY + 10] = 0.0;	// wx
-	    yref_sign[k * NY + 11] = 0.0;	// wy
-	    yref_sign[k * NY + 12] = 0.0;	// wz
-	    yref_sign[k * NY + 13] = uss;	// w1
-	    yref_sign[k * NY + 14] = uss;	// w2
-	    yref_sign[k * NY + 15] = uss;	// w3
-	    yref_sign[k * NY + 16] = uss;	// w4
-      }
+//       if (e.current_real.toSec() - t0 < 5.0){
+// 	// Storing inertial positions in state vector
+// 	x0_sign[xq] = actual_x;
+// 	x0_sign[yq] = actual_y;
+// 	x0_sign[zq] = actual_z;
+// 
+// 	// Get the euler angles from the onboard stabilizer
+// 	eu.phi   = deg2Rad(actual_roll);
+// 	eu.theta = deg2Rad(actual_pitch);
+// 	eu.psi   = deg2Rad(actual_yaw);
+// 
+// 	// Convert IMU euler angles to quaternion
+// 	Quaterniond q_imu = euler2quatern(eu);
+// 
+// 	x0_sign[q1] = q_imu.w();
+// 	x0_sign[q2] = q_imu.x();
+// 	x0_sign[q3] = q_imu.y();
+// 	x0_sign[q4] = q_imu.z();
+// 
+// 	estimateWordLinearVelocities(dt,t0);
+// 
+// 	Vector3d vb_mat;
+// 	vb_mat = rotateLinearVeloE2B(&q_imu);
+// 
+// 	// overwriting linear velocities in the body frame in state vector
+// 	x0_sign[vbx] = vb_mat[0];
+// 	x0_sign[vby] = vb_mat[1];
+// 	x0_sign[vbz] = vb_mat[2];
+// 
+// 	// Storing body angular velocities in state vector
+// 	x0_sign[wx] = actual_wx;
+// 	x0_sign[wy] = actual_wy;
+// 	x0_sign[wz] = actual_wz;
+// 	
+//       }
       
-      //ROS_INFO_STREAM("Current acados regulation point: " << yref_sign[0] << ", "<< yref_sign[1] << ", " << yref_sign[2] << endl);
+//      if (e.current_real.toSec() - t0 > 5.0){
+        if (1) {
+	// Update reference
+	for (k = 0; k < N+1; k++) {
+	      yref_sign[k * NY + 0] = xq_des; 	// xq
+	      yref_sign[k * NY + 1] = yq_des;	// yq
+	      yref_sign[k * NY + 2] = zq_des;	// zq
+	      yref_sign[k * NY + 3] = 1.0;	// q1
+	      yref_sign[k * NY + 4] = 0.0;	// q2
+	      yref_sign[k * NY + 5] = 0.0;	// q3
+	      yref_sign[k * NY + 6] = 0.0;	// q4
+	      yref_sign[k * NY + 7] = 0.0;	// vbx
+	      yref_sign[k * NY + 8] = 0.0;	// vby
+	      yref_sign[k * NY + 9] = 0.0;	// vbz
+	      yref_sign[k * NY + 10] = 0.0;	// wx
+	      yref_sign[k * NY + 11] = 0.0;	// wy
+	      yref_sign[k * NY + 12] = 0.0;	// wz
+	      yref_sign[k * NY + 13] = uss;	// w1
+	      yref_sign[k * NY + 14] = uss;	// w2
+	      yref_sign[k * NY + 15] = uss;	// w3
+	      yref_sign[k * NY + 16] = uss;	// w4
+	}
 
-      for (ii = 0; ii < (NY*NY); ii++) {
-	    acados_in.W[ii] = 0.0;
-      }
-      for (ii = 0; ii < (NYN*NYN); ii++) {
-	    acados_in.WN[ii] = 0.0;
-      }
+	for (ii = 0; ii < (NY*NY); ii++) {
+	      acados_in.W[ii] = 0.0;
+	}
+	for (ii = 0; ii < (NYN*NYN); ii++) {
+	      acados_in.WN[ii] = 0.0;
+	}
 
-      acados_in.W[0+0*NY]   = Wdiag_xq;
-      acados_in.W[1+1*NY]   = Wdiag_yq;
-      acados_in.W[2+2*NY]   = Wdiag_zq;
-      acados_in.W[3+3*NY]   = Wdiag_q1;
-      acados_in.W[4+4*NY]   = Wdiag_q2;
-      acados_in.W[5+5*NY]   = Wdiag_q3;
-      acados_in.W[6+6*NY]   = Wdiag_q4;
-      acados_in.W[7+7*NY]   = Wdiag_vbx;
-      acados_in.W[8+8*NY]   = Wdiag_vby;
-      acados_in.W[9+9*NY]   = Wdiag_vbz;
-      acados_in.W[10+10*NY] = Wdiag_wx;
-      acados_in.W[11+11*NY] = Wdiag_wy;
-      acados_in.W[12+12*NY] = Wdiag_wz;
-      acados_in.W[13+13*NY] = Wdiag_w1;
-      acados_in.W[14+14*NY] = Wdiag_w2;
-      acados_in.W[15+15*NY] = Wdiag_w3;
-      acados_in.W[16+16*NY] = Wdiag_w4;
+	acados_in.W[0+0*NY]   = Wdiag_xq;
+	acados_in.W[1+1*NY]   = Wdiag_yq;
+	acados_in.W[2+2*NY]   = Wdiag_zq;
+	acados_in.W[3+3*NY]   = Wdiag_q1;
+	acados_in.W[4+4*NY]   = Wdiag_q2;
+	acados_in.W[5+5*NY]   = Wdiag_q3;
+	acados_in.W[6+6*NY]   = Wdiag_q4;
+	acados_in.W[7+7*NY]   = Wdiag_vbx;
+	acados_in.W[8+8*NY]   = Wdiag_vby;
+	acados_in.W[9+9*NY]   = Wdiag_vbz;
+	acados_in.W[10+10*NY] = Wdiag_wx;
+	acados_in.W[11+11*NY] = Wdiag_wy;
+	acados_in.W[12+12*NY] = Wdiag_wz;
+	acados_in.W[13+13*NY] = Wdiag_w1;
+	acados_in.W[14+14*NY] = Wdiag_w2;
+	acados_in.W[15+15*NY] = Wdiag_w3;
+	acados_in.W[16+16*NY] = Wdiag_w4;
 
-      acados_in.WN[0+0*(NYN)]   = Wdiag_xq;
-      acados_in.WN[1+1*(NYN)]   = Wdiag_yq;
-      acados_in.WN[2+2*(NYN)]   = Wdiag_zq;
-      acados_in.WN[3+3*(NYN)]   = Wdiag_q1;
-      acados_in.WN[4+4*(NYN)]   = Wdiag_q2;
-      acados_in.WN[5+5*(NYN)]   = Wdiag_q3;
-      acados_in.WN[6+6*(NYN)]   = Wdiag_q4;
-      acados_in.WN[7+7*(NYN)]   = Wdiag_vbx;
-      acados_in.WN[8+8*(NYN)]   = Wdiag_vby;
-      acados_in.WN[9+9*(NYN)]   = Wdiag_vbz;
-      acados_in.WN[10+10*(NYN)] = Wdiag_wx;
-      acados_in.WN[11+11*(NYN)] = Wdiag_wy;
-      acados_in.WN[12+12*(NYN)] = Wdiag_wz;
+	acados_in.WN[0+0*(NYN)]   = 10*Wdiag_xq;
+	acados_in.WN[1+1*(NYN)]   = 10*Wdiag_yq;
+	acados_in.WN[2+2*(NYN)]   = 10*Wdiag_zq;
+	acados_in.WN[3+3*(NYN)]   = 10*Wdiag_q1;
+	acados_in.WN[4+4*(NYN)]   = 10*Wdiag_q2;
+	acados_in.WN[5+5*(NYN)]   = 10*Wdiag_q3;
+	acados_in.WN[6+6*(NYN)]   = 10*Wdiag_q4;
+	acados_in.WN[7+7*(NYN)]   = 10*Wdiag_vbx;
+	acados_in.WN[8+8*(NYN)]   = 10*Wdiag_vby;
+	acados_in.WN[9+9*(NYN)]   = 10*Wdiag_vbz;
+	acados_in.WN[10+10*(NYN)] = 10*Wdiag_wx;
+	acados_in.WN[11+11*(NYN)] = 10*Wdiag_wy;
+	acados_in.WN[12+12*(NYN)] = 10*Wdiag_wz;
 
-      // Storing inertial positions in state vector
-      x0_sign[xq] = actual_x;
-      x0_sign[yq] = actual_y;
-      x0_sign[zq] = actual_z;
+	// Storing inertial positions in state vector
+	x0_sign[xq] = actual_x;
+	x0_sign[yq] = actual_y;
+	x0_sign[zq] = actual_z;
 
-      // Get the euler angles from the onboard stabilizer
-      eu.phi   = deg2Rad(actual_roll);
-      eu.theta = deg2Rad(actual_pitch);
-      eu.psi   = deg2Rad(actual_yaw);
+	// Get the euler angles from the onboard stabilizer
+	eu.phi   = deg2Rad(actual_roll);
+	eu.theta = deg2Rad(actual_pitch);
+	eu.psi   = deg2Rad(actual_yaw);
 
-      // Convert IMU euler angles to quaternion
-      Quaterniond q_imu = euler2quatern(eu);
+	// Convert IMU euler angles to quaternion
+	Quaterniond q_imu = euler2quatern(eu);
 
-      x0_sign[q1] = q_imu.w();
-      x0_sign[q2] = q_imu.x();
-      x0_sign[q3] = q_imu.y();
-      x0_sign[q4] = q_imu.z();
+	x0_sign[q1] = q_imu.w();
+	x0_sign[q2] = q_imu.x();
+	x0_sign[q3] = q_imu.y();
+	x0_sign[q4] = q_imu.z();
 
-      estimateWordLinearVelocities(dt,t0);
+	estimateWordLinearVelocities(dt,t0);
 
-      Vector3d vb_mat;
-      vb_mat = rotateLinearVeloE2B(&q_imu);
+	Vector3d vb_mat;
+	vb_mat = rotateLinearVeloE2B(&q_imu);
 
-      // overwriting linear velocities in the body frame in state vector
-      x0_sign[vbx] = vb_mat[0];
-      x0_sign[vby] = vb_mat[1];
-      x0_sign[vbz] = vb_mat[2];
+	// overwriting linear velocities in the body frame in state vector
+	x0_sign[vbx] = vb_mat[0];
+	x0_sign[vby] = vb_mat[1];
+	x0_sign[vbz] = vb_mat[2];
 
-      // Storing body angular velocities in state vector
-      x0_sign[wx] = actual_wx;
-      x0_sign[wy] = actual_wy;
-      x0_sign[wz] = actual_wz;
+	// Storing body angular velocities in state vector
+	x0_sign[wx] = actual_wx;
+	x0_sign[wy] = actual_wy;
+	x0_sign[wz] = actual_wz;
+	
+	
+	// Up to this point we already stored the 13 states required for the NMPC. So advertise them!
+	geometry_msgs::Quaternion cf_st_quat; // publisher for quaternion
+	geometry_msgs::Vector3    cf_st_lvb;  // publisher for the linear velocities w.r.t. the body frame
+	geometry_msgs::Vector3    cf_st_avb;  // publisher for the angular velocities w.r.t. the body frame
+	
+	
+	cf_st_quat.w = x0_sign[q1];
+	cf_st_quat.x = x0_sign[q2];
+	cf_st_quat.y = x0_sign[q3];
+	cf_st_quat.z = x0_sign[q4];
+	cf_st_lvb.x  = x0_sign[vbx];
+	cf_st_lvb.y  = x0_sign[vby];
+	cf_st_lvb.z  = x0_sign[vbz];
+	cf_st_avb.x  = x0_sign[wx];
+	cf_st_avb.y  = x0_sign[wy];
+	cf_st_avb.z  = x0_sign[wz];
+	
+	m_cf_quat.publish(cf_st_quat);
+	m_cf_lvb.publish(cf_st_lvb);
+	m_cf_avb.publish(cf_st_avb);
+	
+	//---------------------------------------//
+	//				       //
+	//		acados NMPC 	       //
+	//				       //
+	//---------------------------------------//
 
-      // copy signals into local buffers
-      for (i = 0; i < NX; i++){
-	acados_in.x0[i] = x0_sign[i];
-	//cout << "x0: " << acados_in.x0[i] << endl;
-      }
 
-      for (i = 0; i < N; i++) {
-	  for (j = 0; j < NY; ++j) {
-		  acados_in.yref[i*NY + j] = yref_sign[i*NY + j];
-		  //cout <<  "yref: " << acados_in.yref[i] << endl;
+	// copy signals into local buffers
+	for (i = 0; i < NX; i++){
+	  acados_in.x0[i] = x0_sign[i];
+	  //cout << "x0: " << acados_in.x0[i] << endl;
+	}
+
+	for (i = 0; i < N; i++) {
+	    for (j = 0; j < NY; ++j) {
+		    acados_in.yref[i*NY + j] = yref_sign[i*NY + j];
+		    //cout <<  "yref: " << acados_in.yref[i] << endl;
+	    }
+	}
+
+	for (i = 0; i < NYN; i++) {
+	    acados_in.yref_e[i] = yref_sign[N*NY + i];
+	}
+
+	// set initial condition
+	ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "lbx", acados_in.x0);
+	ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "ubx", acados_in.x0);
+
+	// update reference
+	for (ii = 0; ii < N; ii++) {
+	    ocp_nlp_cost_model_set(nlp_config, nlp_dims, nlp_in, ii, "yref", acados_in.yref + ii*NY);
+	    // weights
+	    ocp_nlp_cost_model_set(nlp_config, nlp_dims, nlp_in, ii, "W", acados_in.W);
+	}
+
+	ocp_nlp_cost_model_set(nlp_config, nlp_dims, nlp_in, N, "W", acados_in.WN);
+
+	ocp_nlp_cost_model_set(nlp_config, nlp_dims, nlp_in, N, "yref", acados_in.yref_e);
+
+	// call solver
+	acados_status = acados_solve();
+
+	// assign output signals
+	acados_out.status = acados_status;
+	acados_out.KKT_res = (double)nlp_out->inf_norm_res;
+	acados_out.cpu_time = (double)nlp_out->total_time;
+
+	// Get solution
+	ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, 0, "u", (void *)acados_out.u0);
+
+	// Get next state
+	ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, 1, "x", (void *)acados_out.x1);
+	
+	// Get last state
+	ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, N, "x", (void *)acados_out.xN);
+	
+	if(1) {
+	//if(e.current_real.toSec() - t0 > 10.0){
+// 	      ofstream stateLog("state.txt", std::ios_base::app | std::ios_base::out);
+// 	      // Log state at this point
+// 	      if (stateLog.is_open()){
+// 
+// 		stateLog << acados_in.x0[xq] << " ";
+// 		stateLog << acados_in.x0[yq] << " ";
+// 		stateLog << acados_in.x0[zq] << " ";
+// 		stateLog << acados_in.x0[q1] << " ";
+// 		stateLog << acados_in.x0[q2] << " ";
+// 		stateLog << acados_in.x0[q3] << " ";
+// 		stateLog << acados_in.x0[q4] << " ";
+// 		stateLog << acados_in.x0[vbx] << " ";
+// 		stateLog << acados_in.x0[vby] << " ";
+// 		stateLog << acados_in.x0[vbz] << " ";
+// 		stateLog << acados_in.x0[wx] << " ";
+// 		stateLog << acados_in.x0[wy] << " ";
+// 		stateLog << acados_in.x0[wz] << " ";
+// 		stateLog << endl;
+// 		
+// 		stateLog.close();
+// 	      }
+// 	      
+ 	  for(ii=0; ii< N; ii++){
+	    
+	      ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, ii, "x", (void *)(acados_out.xAllStages));
+	            
+	    // Log open-loop trajectory
+	    ofstream trajLog("traj_openloop.txt", std::ios_base::app | std::ios_base::out);
+
+	    if (trajLog.is_open()){
+	      trajLog << acados_out.xAllStages[xq] << " ";
+	      trajLog << acados_out.xAllStages[yq] << " ";
+	      trajLog << acados_out.xAllStages[zq] << " ";
+	      trajLog << acados_out.xAllStages[q1] << " ";
+	      trajLog << acados_out.xAllStages[q2] << " ";
+	      trajLog << acados_out.xAllStages[q3] << " ";
+	      trajLog << acados_out.xAllStages[q4] << " ";
+	      trajLog << acados_out.xAllStages[vbx] << " ";
+	      trajLog << acados_out.xAllStages[vby] << " ";
+	      trajLog << acados_out.xAllStages[vbz] << " ";
+	      trajLog << acados_out.xAllStages[wx] << " ";
+	      trajLog << acados_out.xAllStages[wy] << " ";
+	      trajLog << acados_out.xAllStages[wz] << " ";
+	      trajLog << endl;
+	      
+	      trajLog.close();
+	    }      
 	  }
+	}
+	
+	// Publish acados position
+	geometry_msgs::Vector3 _acadosState;
+	_acadosState.x = acados_out.x1[xq];
+	_acadosState.y = acados_out.xN[zq];
+	_acadosState.z = acados_out.x1[zq];  
+	m_acados_position.publish(_acadosState);
+		
+	// Publish acados output
+	geometry_msgs::Quaternion _acadosOut; 
+	_acadosOut.w =  acados_out.u0[w1];
+	_acadosOut.x =  acados_out.u0[w2];
+	_acadosOut.y =  acados_out.u0[w3];
+	_acadosOut.z =  acados_out.u0[w4];	 	  
+	m_motvel_pub.publish(_acadosOut);	
+
+
+	// Select the set of optimal states to calculate the real cf control inputs
+	Quaterniond q_acados_out;
+	q_acados_out.w() = acados_out.x1[q1];
+	q_acados_out.x() = acados_out.x1[q2];
+	q_acados_out.y() = acados_out.x1[q3];
+	q_acados_out.z() = acados_out.x1[q4];
+
+
+	// Convert acados output quaternion to desired euler angles
+	euler eu_imu;
+	eu_imu = quatern2euler(&q_acados_out);
+
+	// Publish real control inputs
+	geometry_msgs::Twist msg;
+	msg.linear.x  = -rad2Deg(eu_imu.theta);
+	msg.linear.y  = rad2Deg(eu_imu.phi);
+	msg.linear.z  = krpm2pwm((acados_out.u0[w1]+acados_out.u0[w2]+acados_out.u0[w3]+acados_out.u0[w4])/4);
+	msg.angular.z  = rad2Deg(acados_out.x1[wz]);
+
+	m_pubNav.publish(msg);
+		
+// 	ofstream dataLog("data_log.txt", std::ios_base::app | std::ios_base::out);
+// 
+// 	if (dataLog.is_open()){
+// 
+// 	  dataLog << x0_sign[xq] << " ";
+// 	  dataLog << x0_sign[yq] << " ";
+// 	  dataLog << x0_sign[zq] << " ";
+// 	  dataLog << x0_sign[q1] << " ";
+// 	  dataLog << x0_sign[q2] << " ";
+// 	  dataLog << x0_sign[q3] << " ";
+// 	  dataLog << x0_sign[q4] << " ";
+// 	  dataLog << x0_sign[vbx] << " ";
+// 	  dataLog << x0_sign[vby] << " ";
+// 	  dataLog << x0_sign[vbz] << " ";
+// 	  dataLog << x0_sign[wx] << " ";
+// 	  dataLog << x0_sign[wy] << " ";
+// 	  dataLog << x0_sign[wz] << " ";
+// 	  dataLog << endl;
+// 	  
+// 	  dataLog.close();
+// 	}
       }
-
-      for (i = 0; i < NYN; i++) {
-	  acados_in.yref_e[i] = yref_sign[N*NY + i];
-      }
-
-      // set initial condition
-      ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "lbx", acados_in.x0);
-      ocp_nlp_constraints_model_set(nlp_config, nlp_dims, nlp_in, 0, "ubx", acados_in.x0);
-
-      // update reference
-      for (ii = 0; ii < N; ii++) {
-		  ocp_nlp_cost_model_set(nlp_config, nlp_dims, nlp_in, ii, "yref", acados_in.yref + ii*NY);
-		  // weights
-		  ocp_nlp_cost_model_set(nlp_config, nlp_dims, nlp_in, ii, "W", acados_in.W);
-      }
-
-      ocp_nlp_cost_model_set(nlp_config, nlp_dims, nlp_in, N, "W", acados_in.WN);
-
-      ocp_nlp_cost_model_set(nlp_config, nlp_dims, nlp_in, N, "yref", acados_in.yref_e);
-
-      // call solver
-      acados_status = acados_solve();
-
-      // assign output signals
-      acados_out.status = acados_status;
-      acados_out.KKT_res = (double)nlp_out->inf_norm_res;
-      acados_out.cpu_time = (double)nlp_out->total_time;
-
-
-      // get solution
-      ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, 0, "u", (void *)acados_out.u0);
-
-      // get next state
-      ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, 1, "x", (void *)acados_out.x1);
-
-      // Select the set of optimal state to calculate the cf control inputs
-      Quaterniond q_acados_out;
-      q_acados_out.w() = acados_out.x1[q1];
-      q_acados_out.x() = acados_out.x1[q2];
-      q_acados_out.y() = acados_out.x1[q3];
-      q_acados_out.z() = acados_out.x1[q4];
-
-
-      euler eu_imu;
-      eu_imu = quatern2euler(&q_acados_out);
-
-      geometry_msgs::Twist msg;
-      msg.linear.x  = rad2Deg(eu_imu.theta);
-      msg.linear.y  = rad2Deg(eu_imu.phi);
-      msg.linear.z  = krpm2pwm((acados_out.u0[w1]+acados_out.u0[w2]+acados_out.u0[w3]+acados_out.u0[w4])/4);
-      msg.angular.z  = rad2Deg(acados_out.x1[wz]);
-
-      m_pubNav.publish(msg);
-
-    }
-
- #endif
+   }
 
 private:
     ros::Publisher m_cf_quat;
@@ -658,6 +818,9 @@ private:
     std::vector<double> vy_filter_samples;
     std::vector<double> vz_filter_samples;
     double t0;
+    
+    std::vector<double> _log;
+    int LOG_ONCE;
 
     // Variables of the nmpc control process
     double x0_sign[NX];
