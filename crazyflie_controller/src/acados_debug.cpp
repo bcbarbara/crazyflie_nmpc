0e62efa0dbbae8dcd4bdb8f010bcc1c3cad7295e
Tommaso Sartor <tmmsartor@gmail.com>
2019-09-11 20:13:11 +0200

    log: fix txt log truncate mode

--- crazyflie_controller/src/acados_debug.cpp
+++ crazyflie_controller/src/acados_debug.cpp
@@ -85,6 +85,14 @@ public:
 
     NMPC(const ros::NodeHandle& n, const std::string& ref_traj){
 
+	// print log header
+	ofstream trajLog("openloop_traj.txt", std::ios_base::trunc | std::ios_base::out);
+	if (trajLog.is_open()) trajLog << "trajLog" << endl;
+	trajLog.close();
+	ofstream motorsLog("full_log.txt", std::ios_base::trunc | std::ios_base::out);
+	if (trajLog.is_open()) motorsLog << "motorsLog" << endl;
+	motorsLog.close();
+
 	int status = 0;
 	status = acados_create();
 
@@ -713,7 +721,7 @@ public:
 	   ocp_nlp_out_get(nlp_config, nlp_dims, nlp_out, ii, "x", (void *)(acados_out.xAllStages));
 
  	  // Log open-loop trajectory
-	  ofstream trajLog("openloop_traj.txt", std::ios_base::trunc | std::ios_base::out);
+	  ofstream trajLog("openloop_traj.txt", std::ios_base::app | std::ios_base::out);
 
 	  if (trajLog.is_open()){
 	    trajLog << acados_out.xAllStages[xq] << " ";
@@ -736,7 +744,7 @@ public:
 	}
 	
  	// Log current state x0 and acados output x1 and x2
-	ofstream motorsLog("full_log.txt", std::ios_base::trunc | std::ios_base::out);
+	ofstream motorsLog("full_log.txt", std::ios_base::app | std::ios_base::out);
 
 	if (motorsLog.is_open()){
 	  motorsLog << actual_roll << " ";
