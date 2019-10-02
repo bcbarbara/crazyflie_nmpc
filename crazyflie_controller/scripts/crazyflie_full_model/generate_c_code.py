rotation fix

crazyflie_controller/scripts/crazyflie_full_model/generate_c_code.py
@@ -6,7 +6,7 @@ import scipy.linalg
 from ctypes import *
 from os.path import dirname, join, abspath
 
-ACADOS_PATH = join(dirname(abspath(__file__)), "../../../acados/")
+ACADOS_PATH = join(dirname(abspath(__file__)), "../../../acados")
 
 # create render arguments
 ra = acados_ocp_nlp()
@@ -135,6 +135,7 @@ ra.solver_config.nlp_solver_type = 'SQP_RTI'
 # set header path
 ra.acados_include_path  = f'{ACADOS_PATH}/include'
 ra.acados_lib_path      = f'{ACADOS_PATH}/lib'
+print(ra.acados_include_path)
 
 acados_solver = generate_solver(model, ra, json_file = 'acados_ocp.json')
 