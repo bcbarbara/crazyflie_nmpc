bb452e15799de85f4a6cf649f6bc5622d5d3337b
Tommaso Sartor <tmmsartor@gmail.com>
2019-11-12 16:07:11 +0000

    ros: Add dynamic reconfigure config for fixed_obs case

--- crazyflie_controller/src/acados_fixed_obs.cpp
+++ crazyflie_controller/src/acados_fixed_obs.cpp
@@ -18,7 +18,8 @@
 #include <boost/thread.hpp>
 #include "boost/thread/mutex.hpp"
 // crazyflie
-#include <crazyflie_controller/crazyflie_paramsCollisionAvoidanceConfig.h>
+#include <crazyflie_controller/crazyflie_params_fixed_obsConfig.h>
+// #include <crazyflie_controller/crazyflie_params_fixed_obsConfig.h>
 
 // Matrices and vectors
 #include <eigen3/Eigen/Dense>
@@ -266,15 +267,15 @@ public:
 		{
 		ROS_DEBUG("Setting up the dynamic reconfigure panel and server");
 
-			dynamic_reconfigure::Server<crazyflie_controller::crazyflie_paramsCollisionAvoidanceConfig> server;
-			dynamic_reconfigure::Server<crazyflie_controller::crazyflie_paramsCollisionAvoidanceConfig>::CallbackType f;
+			dynamic_reconfigure::Server<crazyflie_controller::crazyflie_params_fixed_obsConfig> server;
+			dynamic_reconfigure::Server<crazyflie_controller::crazyflie_params_fixed_obsConfig>::CallbackType f;
 			f = boost::bind(&NMPC::callback_dynamic_reconfigure, this, _1, _2);
 			server.setCallback(f);
 
 		ros::spin();
 		}
 
-	void callback_dynamic_reconfigure(crazyflie_controller::crazyflie_paramsCollisionAvoidanceConfig &config, uint32_t level)
+	void callback_dynamic_reconfigure(crazyflie_controller::crazyflie_params_fixed_obsConfig &config, uint32_t level)
 		{
 		if (level && CONTROLLER)
 			{
