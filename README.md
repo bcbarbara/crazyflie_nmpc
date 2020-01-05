48b8c79e4e0ec01494327b90d2f30f4dc095a157
bbarroscarlos <barros@diag.uniroma1.it>
2020-01-05 18:41:05 +0100

    readme update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 crazyflie_nmpc
 =============
-![gem](https://img.shields.io/badge/ver -1.0.0-blue)
+![version](https://img.shields.io/badge/version-1.0.0-blue)
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
 
@@ -11,69 +11,52 @@ ROS stack for [Bitcraze Crazyflie](http://www.bitcraze.se/), with the following 
 * Publishes onboard sensors data in ROS standard message formats
 * Includes NMPC controller for regulation and trajectory tracking (if motion capture system is available)
 
-Check the [wiki]() for more informations. Installation instructions for ROS Kinetic can be found [here]().
+Check the [wiki](https://github.com/bcbarbara/crazyflie_nmpc/wiki) for more information, including the installation instructions for ROS Kinetic which can be found [here](https://github.com/bcbarbara/crazyflie_nmpc/wiki/Install-on-Ubuntu-16.04-LTS-with-ROS-Kinetic).
 
 ## Packages
 
-There are seven packages included: acados, crazyflie_cpp, crazyflie_driver, crazyflie_tools, crazyflie_description, crazyflie_controller, and crazyflie_demo.
+There are seven packages included: `acados`, `crazyflie_cpp`, `crazyflie_driver`, `crazyflie_tools`, `crazyflie_description`, `crazyflie_controller`, and `crazyflie_demo`.
 
-#### Crazyflie_Cpp
+#### acados
 
-This package contains a cpp library for the Crazyradio and Crazyflie. It can be used independently of ROS.
+A submodule with fast and embedded solvers for nonlinear optimal control.
 
-#### Crazyflie_driver
-
-This package contains the driver. In order to support multiple Crazyflies with a single Crazyradio, there is crazyflie_server (communicating with all Crazyflies) and crazyflie_add to dynamically add Crazyflies.
-The server does not communicate to any Crazyflie initially, hence crazyflie_add needs to be used.
-
-#### Crazyflie_tools
-
-This package contains tools which are helpful, but not required for normal operation. So far, it just support one tool for scanning for a Crazyflie.
-
-You can find connected Crazyflies using:
-```
-rosrun crazyflie_tools scan
-```
-
-#### Crazyflie_description
-
-This package contains a 3D model of the Crazyflie (1.0). This is for visualization purposes in rviz.
-
-#### Crazyflie_controller
-
-This package contains an efficient and modular implementation of a Nonlinear Model Predictive Control (NMPC) tailored for the Crazyflie's online trajectory generation and tracking problem. A Real-Time Iteration (RTI) scheme through a Sequential Quadratic Programming (SQP) online algorithm is used in order to solve the Nonlinear Program (NLP).
-
-The package also contains a simple PID controller for hovering or waypoint navigation, which has been develop by Wolfgang Honig, and can be used with external motion capture systems, such as VICON.
+- Documentation can be found on [docs.acados.org](https://docs.acados.org/).
+- Forum: in case of any acados-related question  [discourse.acados.org](https://discourse.acados.org/).
 
 
-#### Crazyflie_demo
 
-For teleoperation using a joystick, use:
-```
-roslaunch crazyflie_demo teleop_logitech.launch
-```
-where the uri specifies the uri of your Crazyflie. You can find valid uris using the scan command in the `crazyflie_tools` package.
-> **Note** By default the services for take off and landing in teleoperation mode are disable, while the emergency service is always enable in any type of mode. The button mapping for each service can be found in `scripts/controller.py`
+#### crazyflie_cpp
+
+This submodule contains a cpp library for the Crazyradio and Crazyflie. It can be used independently of ROS. It supports the logging framework streaming data in real-time and the parameter framework adjusting parameters, such as PID
+gains.
+
+#### crazyflie_driver
+
+It contains a ROS wrapper around `crazyflie_cpp`. The logging subsystem is mapped to ROS messages and parameters are mapped to ROS parameters as well.
+
+#### crazyflie_tools
+
+This package has some features such as: find any Crazyflie in range and list the available logging variables and parameters.
+
+#### crazyflie_description
+
+This package contains a 3D model of the Crazyflie 1.0. This is for visualization purposes in rviz.
+
+#### crazyflie_controller
+
+This package contains an efficient and modular implementation of a nonlinear model predictive controller (NMPC) tailored for the Crazyflie's online trajectory generation and tracking problem. The real-time iteration (RTI) scheme, through a sequential quadratic programming (SQP) algorithm, is used in order to solve the nonlinear program (NLP).
+
+The package also contains a PID position controller for hovering or waypoint navigation, which has been developed by Wolfgang Honig.
+
+Both implementations rely on the knownledge of the Crazyflie's position (e.g. though a motion capture system).
 
 
-For launching the node of the NMPC together with a motion capture system (MOCAP) use:
-```
-roslaunch crazyflie_demo mocap_acados.launch
-```
+#### crazyflie_demo
 
-For retrieving the data from the IMU use:
-```
-roslaunch crazyflie_demo imu_feedback.launch uri:=radio://0/80/2M/E7E7E7E7E7
-```
-> **Note** Acceleration: Be careful with acceleration data. Assuming that the Crazyflie is placed in its neutral right-side-up position on a flat surface you will:
-  > *  Measure +9.81 meters per second squared for the Z axis.
-  > *  If the sensor is rolled +90 degrees (left side up), the acceleration should be -9.81 meters per second squared for the Y axis.
-  > *  If the sensor is pitched +90 degrees (front side down), it should read -9.81 meters per second squared for the X axis.
+Contains sample scripts and launch files for teleoperation, hovering, and waypoint navigation for both single and multi Crazyflie cases.
 
-For checking the status of the 13 states which composes the NMPC state vector use:
-```
-roslaunch crazyflie_demo meas_vector.launch
-```
+For more detailed information regarding each individual package, we refere to the `crazyflie_ros` stack [documentation](http://act.usc.edu/publications/Hoenig_Springer_ROS2017.pdf).
 
 ## ROS Features
 
@@ -97,16 +80,16 @@ The following fields are used:
 * angular.z: yawrate [e.g. -200 to 200 degrees/second]
 * linear.z: thrust [10000 to 60000 (mapped to PWM output)]
 
->**Note** See how to tune these parameters for your joystick in `launch/logitech.launch`
+>**Note** See how to tune these parameters for your joystick in `crazyflie_controller/launch/logitech.launch`
 
 ### Publishers
 
-#### euler angles
+#### euler_angles
 * geometry_msgs/Vector3Stamped
 * contains the euler angles from the onboard stabilizer
 * update: 100 ms (time between crazyflie and ROS not synchronized!)
 
-#### motor speeds
+#### motor_speeds
 * crazyflie_driver/GenericLogData
 * contains the individual motor speeds (PWM value)
 * update: 100 ms (time between crazyflie and ROS not synchronized!)
@@ -121,7 +104,7 @@ The following fields are used:
 
 #### temperature
 * sensor_msgs/Temperature
-* From Barometer (10DOF version only) in degree Celcius (Sensor readings might be higher than expected because the PCB warms up; see http://www.bitcraze.se/2014/02/logging-and-parameter-frameworks-turtorial/)
+* From Barometer (10DOF version only) in degree Celcius (Sensor readings might be higher than expected because the PCB warms up; see [here](http://www.bitcraze.se/2014/02/logging-and-parameter-frameworks-turtorial/))
 * update: 100 ms (time between crazyflie and ROS not synchronized!)
 
 #### magnetic_field
--- crazyflie_controller/src/measurements_vector.cpp
+++ crazyflie_controller/src/measurements_vector.cpp
@@ -1,3 +1,30 @@
+/*
+* Copyright 2020 Barbara Barros Carlos, Tommaso Sartor
+*
+* This file is part of crazyflie_nmpc.
+*
+* The MIT License (MIT)
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+
 #include <ros/ros.h>
 #include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/Twist.h>
@@ -26,9 +53,9 @@
 #define NX 	13 	/* Number of differential state variables.  */
 #define NU 	4	/* Number of control inputs. */
 #define NY 	17	/* Number of measurements/references on nodes 0..N-1. */
-#define NYN 	13	/* Number of measurements/references on node N. */
+#define NYN 13	/* Number of measurements/references on node N. */
 
-#define pi      3.14159265358979323846
+#define pi  3.14159265358979323846
 #define g0	9.80665
 
 
@@ -46,259 +73,215 @@ public:
 
     meas_vec(const ros::NodeHandle& n){
 
-        ros::NodeHandle nh;
-	// subscriber for the motion capture system position
-	m_eRaptor_sub  	 	= nh.subscribe("/crazyflie/external_position", 5, &meas_vec::eRaptorCallback, this);
-	// publisher for the IMU stabilizer euler angles
-	m_euler_sub		= nh.subscribe("/crazyflie/euler_angles", 5, &meas_vec::eulerCallback, this);
-	// subscriber for the IMU linear acceleration and angular velocities from acc and gyro
-	m_imu_sub		= nh.subscribe("/crazyflie/imu", 5, &meas_vec::imuCallback, this);
-	// publisher for the current value of the quaternion
-	m_quat_pub 		= nh.advertise<geometry_msgs::Quaternion>("/crazyflie/quat",1);
-	// publisher for the current value of the linear velocities
-	m_lvb_pub 		= nh.advertise<geometry_msgs::Vector3>("/crazyflie/linear_velo",1);
-	// publisher for the current value of the angular velocities
-	m_avb_pub 		= nh.advertise<geometry_msgs::Vector3>("/crazyflie/angular_velo",1);
-	// subscriber fro the motors rpm
-	m_motors 		= nh.subscribe("/crazyflie/log1", 5, &meas_vec::motorsCallback, this);
-	
-	
-	// Set initial value of the linear velocities to zero
-	vx = 0.0;
-	vy = 0.0;
-	vz = 0.0;
+      ros::NodeHandle nh;
+    	// subscriber for the motion capture system position
+    	m_eRaptor_sub  	 	= nh.subscribe("/crazyflie/external_position", 5, &meas_vec::eRaptorCallback, this);
+    	// publisher for the IMU stabilizer euler angles
+    	m_euler_sub		= nh.subscribe("/crazyflie/euler_angles", 5, &meas_vec::eulerCallback, this);
+    	// subscriber for the IMU linear acceleration and angular velocities from acc and gyro
+    	m_imu_sub		= nh.subscribe("/crazyflie/imu", 5, &meas_vec::imuCallback, this);
+    	// publisher for the current value of the quaternion
+    	m_quat_pub 		= nh.advertise<geometry_msgs::Quaternion>("/crazyflie/quat",1);
+    	// publisher for the current value of the linear velocities
+    	m_lvb_pub 		= nh.advertise<geometry_msgs::Vector3>("/crazyflie/linear_velo",1);
+    	// publisher for the current value of the angular velocities
+    	m_avb_pub 		= nh.advertise<geometry_msgs::Vector3>("/crazyflie/angular_velo",1);
+    	// subscriber fro the motors rpm
+    	m_motors 		= nh.subscribe("/crazyflie/log1", 5, &meas_vec::motorsCallback, this);
 
-	// Set size of buffer
-	x_samples.resize(5);
-	y_samples.resize(5);
-	z_samples.resize(5);
-	
-	// Set initial value of the buffer to zero
-	for (unsigned int i = 0; i <= 4; i++) x_samples[i] = 0.0;
-	for (unsigned int i = 0; i <= 4; i++) y_samples[i] = 0.0;
-	for (unsigned int i = 0; i <= 4; i++) z_samples[i] = 0.0;
 
-	// Set size of buffer
-	vx_filter_samples.resize(5);
-	vy_filter_samples.resize(5);
-	vz_filter_samples.resize(5);
+    	// Set initial value of the linear velocities to zero
+    	vx = 0.0;
+    	vy = 0.0;
+    	vz = 0.0;
 
-	for (unsigned int i = 0; i <= 4; i++) vx_filter_samples[i] = 0.0;
-	for (unsigned int i = 0; i <= 4; i++) vy_filter_samples[i] = 0.0;
-	for (unsigned int i = 0; i <= 4; i++) vz_filter_samples[i] = 0.0;
+    	// Set size of buffer
+    	x_samples.resize(5);
+    	y_samples.resize(5);
+    	z_samples.resize(5);
 
-	// Set elapsed time to zero initially
-	t0 = 0.0;
+    	// Set initial value of the buffer to zero
+    	for (unsigned int i = 0; i <= 4; i++) x_samples[i] = 0.0;
+    	for (unsigned int i = 0; i <= 4; i++) y_samples[i] = 0.0;
+    	for (unsigned int i = 0; i <= 4; i++) z_samples[i] = 0.0;
 
+    	// Set size of buffer
+    	vx_filter_samples.resize(5);
+    	vy_filter_samples.resize(5);
+    	vz_filter_samples.resize(5);
+
+    	for (unsigned int i = 0; i <= 4; i++) vx_filter_samples[i] = 0.0;
+    	for (unsigned int i = 0; i <= 4; i++) vy_filter_samples[i] = 0.0;
+    	for (unsigned int i = 0; i <= 4; i++) vz_filter_samples[i] = 0.0;
+
+    	// Set elapsed time to zero initially
+    	t0 = 0.0;
     }
-    
-    void run(double frequency)
-    {
+
+    void run(double frequency){
+
         ros::NodeHandle node;
         ros::Timer timer = node.createTimer(ros::Duration(1.0/frequency), &meas_vec::iteration, this);
-	
+
         ros::spin();
     }
 
 private:
-  
-  enum systemStates{
-	  xq = 0,
-	  yq = 1,
-	  zq = 2,
-	  q1 = 3,
-	  q2 = 4,
-	  q3 = 5,
-	  q4 = 6,
-	  vbx = 7,
-	  vby = 8,
-	  vbz = 9,
-	  wx = 10,
-	  wy = 11,
-	  wz = 12
-    };
-
-  enum controlInputs{
-	w1 = 0,
-	w2 = 1,
-	w3 = 2,
-	w4 = 3
-  };
 
   struct euler{
     double phi;
     double theta;
     double psi;
   };
-  
-    double deg2Rad(double deg) {
-	return deg / 180.0 * pi;
+
+  double deg2Rad(double deg) {
+	   return deg / 180.0 * pi;
   }
 
   double rad2Deg(double rad) {
-	return rad * 180.0 / pi;
+	   return rad * 180.0 / pi;
   }
 
   void motorsCallback(const crazyflie_controller::GenericLogDataConstPtr& msg){
-    
-	// motors rpm
-	actual_m1 = msg->values[0];
-	actual_m2 = msg->values[1];
-	actual_m3 = msg->values[2];
-	actual_m4 = msg->values[3];
+
+  	// motors rpm
+  	actual_m1 = msg->values[0];
+  	actual_m2 = msg->values[1];
+  	actual_m3 = msg->values[2];
+  	actual_m4 = msg->values[3];
   }
-  
+
   void eRaptorCallback(const geometry_msgs::PointStampedConstPtr& msg){
 
-	// Position of crazyflie marker
-	actual_x = msg->point.x;
-	actual_y = msg->point.y;
-	actual_z = msg->point.z;
-    }
+  	// Position of crazyflie marker
+  	actual_x = msg->point.x;
+  	actual_y = msg->point.y;
+  	actual_z = msg->point.z;
+  }
 
-     void eulerCallback(const geometry_msgs::Vector3StampedPtr& msg){
+  void eulerCallback(const geometry_msgs::Vector3StampedPtr& msg){
 
-      	// Euler angles
-	actual_roll  = msg->vector.x;
-	actual_pitch = -msg->vector.y;
-	actual_yaw   = msg->vector.z;
-    }
+    // Euler angles
+  	actual_roll  = msg->vector.x;
+  	actual_pitch = -msg->vector.y;
+  	actual_yaw   = msg->vector.z;
+  }
 
-    void imuCallback(const sensor_msgs::Imu::ConstPtr& msg){
-      
-	// Gyro angular rates
-	actual_wx  = msg->angular_velocity.x;
-	actual_wy  = msg->angular_velocity.y;
-	actual_wz  = msg->angular_velocity.z;
-    }
-    
+  void imuCallback(const sensor_msgs::Imu::ConstPtr& msg){
+
+    	// Gyro angular rates
+    	actual_wx  = msg->angular_velocity.x;
+    	actual_wy  = msg->angular_velocity.y;
+    	actual_wz  = msg->angular_velocity.z;
+  }
+
    Quaterniond euler2quatern(euler angle){
 
-	  Quaterniond q;
+  	  Quaterniond q;
 
-	  double cosPhi = cos(angle.phi*0.5);
-	  double sinPhi = sin(angle.phi*0.5);
+  	  double cosPhi = cos(angle.phi*0.5);
+  	  double sinPhi = sin(angle.phi*0.5);
 
-	  double cosTheta = cos(angle.theta*0.5);
-	  double sinTheta = sin(angle.theta*0.5);
+  	  double cosTheta = cos(angle.theta*0.5);
+  	  double sinTheta = sin(angle.theta*0.5);
 
-	  double cosPsi = cos(angle.psi*0.5);
-	  double sinPsi = sin(angle.psi*0.5);
+  	  double cosPsi = cos(angle.psi*0.5);
+  	  double sinPsi = sin(angle.psi*0.5);
 
-	  // Convention according the firmware of the crazyflie
-	  q.w() = cosPsi*cosTheta*cosPhi + sinPsi*sinTheta*sinPhi;
-	  q.x() = cosPsi*cosTheta*sinPhi - sinPsi*sinTheta*cosPhi;
-	  q.y() = cosPsi*sinTheta*cosPhi + sinPsi*cosTheta*sinPhi;
-	  q.z() = sinPsi*cosTheta*cosPhi - cosPsi*sinTheta*sinPhi;
+  	  // Convention according the firmware of the crazyflie
+  	  q.w() = cosPsi*cosTheta*cosPhi + sinPsi*sinTheta*sinPhi;
+  	  q.x() = cosPsi*cosTheta*sinPhi - sinPsi*sinTheta*cosPhi;
+  	  q.y() = cosPsi*sinTheta*cosPhi + sinPsi*cosTheta*sinPhi;
+  	  q.z() = sinPsi*cosTheta*cosPhi - cosPsi*sinTheta*sinPhi;
 
-	  return q;
+  	  return q;
     }
-    
+
     double linearVelocity(std::vector <double> q_samples, std::vector <double> dq_samples, double Ts, double elapsed_time) {
 
-	double dq = 0;
-	if (elapsed_time > 1.0) dq = 0.1814*dq_samples[4] - 0.00823*dq_samples[3] + 26.13*q_samples[4] - 26.13*q_samples[3];
-	else dq = (q_samples[4] - q_samples[3]) / Ts;
-	return dq;
+    	double dq = 0;
+    	if (elapsed_time > 1.0) dq = 0.1814*dq_samples[4] - 0.00823*dq_samples[3] + 26.13*q_samples[4] - 26.13*q_samples[3];
+    	else dq = (q_samples[4] - q_samples[3]) / Ts;
+    	return dq;
     }
 
     Vector3d estimateWordLinearVelocities(float dt, float delta){
-      
-	  Vector3d vearth;
-      
-	  //estimte the velocity
-	  x_samples[0] = x_samples[1];
-	  x_samples[1] = x_samples[2];
-	  x_samples[2] = x_samples[3];
-	  x_samples[3] = x_samples[4];
-	  x_samples[4] = actual_x;
-	  y_samples[0] = y_samples[1];
-	  y_samples[1] = y_samples[2];
-	  y_samples[2] = y_samples[3];
-	  y_samples[3] = y_samples[4];
-	  y_samples[4] = actual_y;
-	  z_samples[0] = z_samples[1];
-	  z_samples[1] = z_samples[2];
-	  z_samples[2] = z_samples[3];
-	  z_samples[3] = z_samples[4];
-	  z_samples[4] = actual_z;
 
-	  vearth[0] = linearVelocity(x_samples, vx_filter_samples, dt, delta);
-	  vearth[1] = linearVelocity(y_samples, vy_filter_samples, dt, delta);
-	  vearth[2] = linearVelocity(z_samples, vz_filter_samples, dt, delta);
+  	  Vector3d vearth;
 
-	  vx_filter_samples[0] = vx_filter_samples[1];
-	  vx_filter_samples[1] = vx_filter_samples[2];
-	  vx_filter_samples[2] = vx_filter_samples[3];
-	  vx_filter_samples[3] = vx_filter_samples[4];
-	  vx_filter_samples[4] = vearth[0];
-	  vy_filter_samples[0] = vy_filter_samples[1];
-	  vy_filter_samples[1] = vy_filter_samples[2];
-	  vy_filter_samples[2] = vy_filter_samples[3];
-	  vy_filter_samples[3] = vy_filter_samples[4];
-	  vy_filter_samples[4] = vearth[1];
-	  vz_filter_samples[0] = vz_filter_samples[1];
-	  vz_filter_samples[1] = vz_filter_samples[2];
-	  vz_filter_samples[2] = vz_filter_samples[3];
-	  vz_filter_samples[3] = vz_filter_samples[4];
-	  vz_filter_samples[4] = vearth[2];
-	  
-	  return vearth;
+  	  //estimte the velocity
+  	  x_samples[0] = x_samples[1];
+  	  x_samples[1] = x_samples[2];
+  	  x_samples[2] = x_samples[3];
+  	  x_samples[3] = x_samples[4];
+  	  x_samples[4] = actual_x;
+  	  y_samples[0] = y_samples[1];
+  	  y_samples[1] = y_samples[2];
+  	  y_samples[2] = y_samples[3];
+  	  y_samples[3] = y_samples[4];
+  	  y_samples[4] = actual_y;
+  	  z_samples[0] = z_samples[1];
+  	  z_samples[1] = z_samples[2];
+  	  z_samples[2] = z_samples[3];
+  	  z_samples[3] = z_samples[4];
+  	  z_samples[4] = actual_z;
+
+  	  vearth[0] = linearVelocity(x_samples, vx_filter_samples, dt, delta);
+  	  vearth[1] = linearVelocity(y_samples, vy_filter_samples, dt, delta);
+  	  vearth[2] = linearVelocity(z_samples, vz_filter_samples, dt, delta);
+
+  	  vx_filter_samples[0] = vx_filter_samples[1];
+  	  vx_filter_samples[1] = vx_filter_samples[2];
+  	  vx_filter_samples[2] = vx_filter_samples[3];
+  	  vx_filter_samples[3] = vx_filter_samples[4];
+  	  vx_filter_samples[4] = vearth[0];
+  	  vy_filter_samples[0] = vy_filter_samples[1];
+  	  vy_filter_samples[1] = vy_filter_samples[2];
+  	  vy_filter_samples[2] = vy_filter_samples[3];
+  	  vy_filter_samples[3] = vy_filter_samples[4];
+  	  vy_filter_samples[4] = vearth[1];
+  	  vz_filter_samples[0] = vz_filter_samples[1];
+  	  vz_filter_samples[1] = vz_filter_samples[2];
+  	  vz_filter_samples[2] = vz_filter_samples[3];
+  	  vz_filter_samples[3] = vz_filter_samples[4];
+  	  vz_filter_samples[4] = vearth[2];
+
+  	  return vearth;
     }
 
     Vector3d rotateLinearVeloE2B(Quaterniond* q, Vector3d v_inertial){
 
-	 // This is the convertion between
-	 // quaternion orientation to rotation matrix
-	 // from BODY to EARTH according the crazyflie firmware
-// 	 Matrix3d Sq;
-// 	 Vector3d vb;
-// 
-// 	 double S11 = 2*(q->w()*q->w()+q->x()*q->x())-1;;
-// 	 double S12 = 2*(q->x()*q->y()-q->w()*q->z());
-// 	 double S13 = 2*(q->x()*q->z()+q->w()*q->y());
-// 	 double S21 = 2*(q->x()*q->y()+q->w()*q->z());
-// 	 double S22 = 2*(q->w()*q->w()+q->y()*q->y())-1;
-// 	 double S23 = 2*(q->y()*q->z()-q->w()*q->x());
-// 	 double S31 = 2*(q->x()*q->z()-q->w()*q->y());
-// 	 double S32 = 2*(q->y()*q->z()+q->w()*q->x());
-// 	 double S33 = 2*(q->w()*q->w()+q->z()*q->z())-1;
-      
-         // This is the convertion between
-	 // quaternion orientation to rotation matrix
-	 // from EARTH to BODY (considering ZYX euler sequence)
- 	 Matrix3d Sq;
- 	 Vector3d vb;
+     	 Matrix3d Sq;
+     	 Vector3d vb;
 
-	 double S11 = 2*(q->w()*q->w()+q->x()*q->x())-1;
-	 double S12 = 2*(q->x()*q->y()+q->w()*q->z());
-	 double S13 = 2*(q->x()*q->z()-q->w()*q->y());
-	 double S21 = 2*(q->x()*q->y()-q->w()*q->z());
-	 double S22 = 2*(q->w()*q->w()+q->y()*q->y())-1;
-	 double S23 = 2*(q->y()*q->z()+q->w()*q->x());
-	 double S31 = 2*(q->x()*q->z()+q->w()*q->y());
-	 double S32 = 2*(q->y()*q->z()-q->w()*q->x());
-	 double S33 = 2*(q->w()*q->w()+q->z()*q->z())-1;
+    	 double S11 = 2*(q->w()*q->w()+q->x()*q->x())-1;
+    	 double S12 = 2*(q->x()*q->y()+q->w()*q->z());
+    	 double S13 = 2*(q->x()*q->z()-q->w()*q->y());
+    	 double S21 = 2*(q->x()*q->y()-q->w()*q->z());
+    	 double S22 = 2*(q->w()*q->w()+q->y()*q->y())-1;
+    	 double S23 = 2*(q->y()*q->z()+q->w()*q->x());
+    	 double S31 = 2*(q->x()*q->z()+q->w()*q->y());
+    	 double S32 = 2*(q->y()*q->z()-q->w()*q->x());
+    	 double S33 = 2*(q->w()*q->w()+q->z()*q->z())-1;
 
 
-	 Sq << S11,S12,S13,
-	       S21,S22,S23,
-	       S31,S32,S33;
+    	 Sq << S11,S12,S13,
+    	       S21,S22,S23,
+    	       S31,S32,S33;
 
-	 vb = Sq*v_inertial;
-	 
-	 return vb;
+    	 vb = Sq*v_inertial;
+
+    	 return vb;
     }
-    
+
     void iteration(const ros::TimerEvent& e){
-	  
+
       if(e.last_real.isZero()) {
-	t0 = e.current_real.toSec();
-	//return;
+	       t0 = e.current_real.toSec();
       }
-	
-      double dt = e.current_real.toSec() - e.last_real.toSec();   
-        
+
+      double dt = e.current_real.toSec() - e.last_real.toSec();
+
       // Storing inertial positions in state vector
       x0_sign[xq] = actual_x;
       x0_sign[yq] = actual_y;
@@ -309,15 +292,15 @@ private:
       eu.phi   = deg2Rad(actual_roll);
       eu.theta = deg2Rad(actual_pitch);
       eu.psi   = deg2Rad(actual_yaw);
-      
+
       // Convert IMU euler angles to quaternion
       Quaterniond q_imu = euler2quatern(eu);
       q_imu.normalize();
 
-      x0_sign[q1] = q_imu.w();
-      x0_sign[q2] = q_imu.x();
-      x0_sign[q3] = q_imu.y();
-      x0_sign[q4] = q_imu.z();
+      x0_sign[qw] = q_imu.w();
+      x0_sign[qx] = q_imu.x();
+      x0_sign[qy] = q_imu.y();
+      x0_sign[qz] = q_imu.z();
 
       Vector3d vi_mat;
       vi_mat = estimateWordLinearVelocities(dt,t0);
@@ -334,62 +317,30 @@ private:
       x0_sign[wx] = actual_wx;
       x0_sign[wy] = actual_wy;
       x0_sign[wz] = actual_wz;
-      
-      
+
+
       // Up to this point we already stored the 13 states required for the NMPC. So advertise them!
       geometry_msgs::Quaternion cf_st_quat; // publisher for quaternion
       geometry_msgs::Vector3    cf_st_lvb;  // publisher for the linear velocities w.r.t. the body frame
       geometry_msgs::Vector3    cf_st_avb;  // publisher for the angular velocities w.r.t. the body frame
-      
-      
-      cf_st_quat.w = x0_sign[q1];
-      cf_st_quat.x = x0_sign[q2];
-      cf_st_quat.y = x0_sign[q3];
-      cf_st_quat.z = x0_sign[q4];
+
+
+      cf_st_quat.w = x0_sign[qw];
+      cf_st_quat.x = x0_sign[qx];
+      cf_st_quat.y = x0_sign[qy];
+      cf_st_quat.z = x0_sign[qz];
       cf_st_lvb.x  = x0_sign[vbx];
       cf_st_lvb.y  = x0_sign[vby];
       cf_st_lvb.z  = x0_sign[vbz];
       cf_st_avb.x  = x0_sign[wx];
       cf_st_avb.y  = x0_sign[wy];
       cf_st_avb.z  = x0_sign[wz];
-      
+
       m_quat_pub.publish(cf_st_quat);
       m_lvb_pub.publish(cf_st_lvb);
       m_avb_pub.publish(cf_st_avb);
-      
-      
-//       ofstream statesLog("states_controls.txt", std::ios_base::app | std::ios_base::out);
-      ofstream motorsLog("motors.txt", std::ios_base::app | std::ios_base::out);
 
-      if (motorsLog.is_open()){
-
-// 	motorsLog << eu.phi      << " ";
-// 	motorsLog << eu.theta    << " ";
-// 	motorsLog << eu.psi      << " ";
-// 	motorsLog << vi_mat[0]   << " ";
-// 	motorsLog << vi_mat[1]   << " ";
-// 	motorsLog << vi_mat[2]   << " ";
-// 	motorsLog << x0_sign[xq] << " ";
-// 	motorsLog << x0_sign[yq] << " ";
-// 	motorsLog << x0_sign[zq] << " ";
-// 	motorsLog << x0_sign[q1] << " ";
-// 	motorsLog << x0_sign[q2] << " ";
-// 	motorsLog << x0_sign[q3] << " ";
-// 	motorsLog << x0_sign[q4] << " ";
-// 	motorsLog << x0_sign[vbx] << " ";
-// 	motorsLog << x0_sign[vby] << " ";
-// 	motorsLog << x0_sign[vbz] << " ";
-// 	motorsLog << x0_sign[wx] << " ";
-// 	motorsLog << x0_sign[wy] << " ";
-// 	motorsLog << x0_sign[wz] << " ";
-	motorsLog << actual_m1 << " ";
-	motorsLog << actual_m2 << " ";
-	motorsLog << actual_m3 << " ";
-	motorsLog << actual_m4 << " ";	
-	motorsLog << endl;
-	
-	motorsLog.close();
-      }	
+      }
     }
 
 private:
@@ -398,7 +349,7 @@ private:
     ros::Subscriber m_euler_sub;
     ros::Subscriber m_eRaptor_sub;
     ros::Subscriber m_motors;
-    
+
     ros::Publisher m_quat_pub;
     ros::Publisher m_lvb_pub;
     ros::Publisher m_avb_pub;
@@ -407,10 +358,10 @@ private:
     double actual_wx,actual_wy,actual_wz;
     double actual_roll,actual_pitch,actual_yaw;
     int32_t actual_m1,actual_m2,actual_m3,actual_m4;
-    
+
     // Variables for the mocap callback
     double actual_x,actual_y,actual_z;
-    
+
     double vx,vy,vz;
     std::vector<double> x_samples;
     std::vector<double> y_samples;
@@ -419,11 +370,11 @@ private:
     std::vector<double> vy_filter_samples;
     std::vector<double> vz_filter_samples;
     double t0;
-    
+
     // Variables of the nmpc control process
     double x0_sign[NX];
     double yref_sign[(NY*N)+NY];
-  
+
 };
 
 int main(int argc, char **argv)
