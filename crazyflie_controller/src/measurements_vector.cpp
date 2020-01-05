8c0d92321e863685a5d4cffc9a1f3659cbb12983
bbarroscarlos <barros@diag.uniroma1.it>
2020-01-05 20:09:04 +0100

    crazyflie_controller: including other nodes into CMakeLists

--- crazyflie_controller/src/measurements_vector.cpp
+++ crazyflie_controller/src/measurements_vector.cpp
@@ -69,9 +69,62 @@ using std::showpos;
 
 class meas_vec
 {
+  enum systemStates{
+    xq = 0,
+    yq = 1,
+    zq = 2,
+    qw = 3,
+    qx = 4,
+    qy = 5,
+    qz = 6,
+    vbx = 7,
+    vby = 8,
+    vbz = 9,
+    wx = 10,
+    wy = 11,
+    wz = 12
+  };
+
+  enum controlInputs{
+    w1 = 0,
+    w2 = 1,
+    w3 = 2,
+    w4 = 3
+  };
+
+  ros::Subscriber m_imu_sub;
+  ros::Subscriber m_euler_sub;
+  ros::Subscriber m_eRaptor_sub;
+  ros::Subscriber m_motors;
+
+  ros::Publisher m_quat_pub;
+  ros::Publisher m_lvb_pub;
+  ros::Publisher m_avb_pub;
+
+  // Variables for reading the IMU data
+  double actual_wx,actual_wy,actual_wz;
+  double actual_roll,actual_pitch,actual_yaw;
+  int32_t actual_m1,actual_m2,actual_m3,actual_m4;
+
+  // Variables for the mocap callback
+  double actual_x,actual_y,actual_z;
+
+  double vx,vy,vz;
+  std::vector<double> x_samples;
+  std::vector<double> y_samples;
+  std::vector<double> z_samples;
+  std::vector<double> vx_filter_samples;
+  std::vector<double> vy_filter_samples;
+  std::vector<double> vz_filter_samples;
+  double t0;
+
+  // Variables of the nmpc control process
+  double x0_sign[NX];
+  double yref_sign[(NY*N)+NY];
+
 public:
 
-    meas_vec(const ros::NodeHandle& n){
+  meas_vec(const ros::NodeHandle& n){
 
       ros::NodeHandle nh;
     	// subscriber for the motion capture system position
@@ -118,15 +171,13 @@ public:
     	t0 = 0.0;
     }
 
-    void run(double frequency){
+  void run(double frequency){
 
         ros::NodeHandle node;
         ros::Timer timer = node.createTimer(ros::Duration(1.0/frequency), &meas_vec::iteration, this);
 
         ros::spin();
     }
-
-private:
 
   struct euler{
     double phi;
@@ -175,206 +226,174 @@ private:
     	actual_wz  = msg->angular_velocity.z;
   }
 
-   Quaterniond euler2quatern(euler angle){
+  double linearVelocity(std::vector <double> q_samples, std::vector <double> dq_samples, double Ts, double elapsed_time) {
 
-  	  Quaterniond q;
+  	double dq = 0;
+    if (elapsed_time > 1.0)
+      dq = 0.3306*dq_samples[4] - 0.02732*dq_samples[3] + 35.7*q_samples[4] - 35.7*q_samples[3];
+    else
+      dq = (q_samples[4] - q_samples[3]) / Ts;
+  	return dq;
+  }
 
-  	  double cosPhi = cos(angle.phi*0.5);
-  	  double sinPhi = sin(angle.phi*0.5);
+  Vector3d estimateWordLinearVelocities(float dt, float delta){
 
-  	  double cosTheta = cos(angle.theta*0.5);
-  	  double sinTheta = sin(angle.theta*0.5);
+	  Vector3d vearth;
 
-  	  double cosPsi = cos(angle.psi*0.5);
-  	  double sinPsi = sin(angle.psi*0.5);
+	  //estimte the velocity
+	  x_samples[0] = x_samples[1];
+	  x_samples[1] = x_samples[2];
+	  x_samples[2] = x_samples[3];
+	  x_samples[3] = x_samples[4];
+	  x_samples[4] = actual_x;
+	  y_samples[0] = y_samples[1];
+	  y_samples[1] = y_samples[2];
+	  y_samples[2] = y_samples[3];
+	  y_samples[3] = y_samples[4];
+	  y_samples[4] = actual_y;
+	  z_samples[0] = z_samples[1];
+	  z_samples[1] = z_samples[2];
+	  z_samples[2] = z_samples[3];
+	  z_samples[3] = z_samples[4];
+	  z_samples[4] = actual_z;
 
-  	  // Convention according the firmware of the crazyflie
-  	  q.w() = cosPsi*cosTheta*cosPhi + sinPsi*sinTheta*sinPhi;
-  	  q.x() = cosPsi*cosTheta*sinPhi - sinPsi*sinTheta*cosPhi;
-  	  q.y() = cosPsi*sinTheta*cosPhi + sinPsi*cosTheta*sinPhi;
-  	  q.z() = sinPsi*cosTheta*cosPhi - cosPsi*sinTheta*sinPhi;
+	  vearth[0] = linearVelocity(x_samples, vx_filter_samples, dt, delta);
+	  vearth[1] = linearVelocity(y_samples, vy_filter_samples, dt, delta);
+	  vearth[2] = linearVelocity(z_samples, vz_filter_samples, dt, delta);
 
-  	  return q;
+	  vx_filter_samples[0] = vx_filter_samples[1];
+	  vx_filter_samples[1] = vx_filter_samples[2];
+	  vx_filter_samples[2] = vx_filter_samples[3];
+	  vx_filter_samples[3] = vx_filter_samples[4];
+	  vx_filter_samples[4] = vearth[0];
+	  vy_filter_samples[0] = vy_filter_samples[1];
+	  vy_filter_samples[1] = vy_filter_samples[2];
+	  vy_filter_samples[2] = vy_filter_samples[3];
+	  vy_filter_samples[3] = vy_filter_samples[4];
+	  vy_filter_samples[4] = vearth[1];
+	  vz_filter_samples[0] = vz_filter_samples[1];
+	  vz_filter_samples[1] = vz_filter_samples[2];
+	  vz_filter_samples[2] = vz_filter_samples[3];
+	  vz_filter_samples[3] = vz_filter_samples[4];
+	  vz_filter_samples[4] = vearth[2];
+
+	  return vearth;
+  }
+
+  Vector3d rotateLinearVeloE2B(Quaterniond* q, Vector3d v_inertial){
+
+   	 Matrix3d Sq;
+   	 Vector3d vb;
+
+  	 double S11 = 2*(q->w()*q->w()+q->x()*q->x())-1;
+  	 double S12 = 2*(q->x()*q->y()+q->w()*q->z());
+  	 double S13 = 2*(q->x()*q->z()-q->w()*q->y());
+  	 double S21 = 2*(q->x()*q->y()-q->w()*q->z());
+  	 double S22 = 2*(q->w()*q->w()+q->y()*q->y())-1;
+  	 double S23 = 2*(q->y()*q->z()+q->w()*q->x());
+  	 double S31 = 2*(q->x()*q->z()+q->w()*q->y());
+  	 double S32 = 2*(q->y()*q->z()-q->w()*q->x());
+  	 double S33 = 2*(q->w()*q->w()+q->z()*q->z())-1;
+
+
+  	 Sq << S11,S12,S13,
+  	       S21,S22,S23,
+  	       S31,S32,S33;
+
+  	 vb = Sq*v_inertial;
+
+  	 return vb;
+  }
+
+  Quaterniond euler2quatern(euler angle){
+
+      Quaterniond q;
+
+      double cosPhi = cos(angle.phi*0.5);
+      double sinPhi = sin(angle.phi*0.5);
+
+      double cosTheta = cos(angle.theta*0.5);
+      double sinTheta = sin(angle.theta*0.5);
+
+      double cosPsi = cos(angle.psi*0.5);
+      double sinPsi = sin(angle.psi*0.5);
+
+      // Convention according the firmware of the crazyflie
+      q.w() = cosPsi*cosTheta*cosPhi + sinPsi*sinTheta*sinPhi;
+      q.x() = cosPsi*cosTheta*sinPhi - sinPsi*sinTheta*cosPhi;
+      q.y() = cosPsi*sinTheta*cosPhi + sinPsi*cosTheta*sinPhi;
+      q.z() = sinPsi*cosTheta*cosPhi - cosPsi*sinTheta*sinPhi;
+
+      return q;
     }
 
-    double linearVelocity(std::vector <double> q_samples, std::vector <double> dq_samples, double Ts, double elapsed_time) {
+  void iteration(const ros::TimerEvent& e){
 
-    	double dq = 0;
-    	if (elapsed_time > 1.0) dq = 0.1814*dq_samples[4] - 0.00823*dq_samples[3] + 26.13*q_samples[4] - 26.13*q_samples[3];
-    	else dq = (q_samples[4] - q_samples[3]) / Ts;
-    	return dq;
+    if(e.last_real.isZero()) {
+       t0 = e.current_real.toSec();
     }
 
-    Vector3d estimateWordLinearVelocities(float dt, float delta){
+    double dt = e.current_real.toSec() - e.last_real.toSec();
 
-  	  Vector3d vearth;
+    // Storing inertial positions in state vector
+    x0_sign[xq] = actual_x;
+    x0_sign[yq] = actual_y;
+    x0_sign[zq] = actual_z;
 
-  	  //estimte the velocity
-  	  x_samples[0] = x_samples[1];
-  	  x_samples[1] = x_samples[2];
-  	  x_samples[2] = x_samples[3];
-  	  x_samples[3] = x_samples[4];
-  	  x_samples[4] = actual_x;
-  	  y_samples[0] = y_samples[1];
-  	  y_samples[1] = y_samples[2];
-  	  y_samples[2] = y_samples[3];
-  	  y_samples[3] = y_samples[4];
-  	  y_samples[4] = actual_y;
-  	  z_samples[0] = z_samples[1];
-  	  z_samples[1] = z_samples[2];
-  	  z_samples[2] = z_samples[3];
-  	  z_samples[3] = z_samples[4];
-  	  z_samples[4] = actual_z;
+    // Get the euler angles from the onboard stabilizer
+    euler eu;
+    eu.phi   = deg2Rad(actual_roll);
+    eu.theta = deg2Rad(actual_pitch);
+    eu.psi   = deg2Rad(actual_yaw);
 
-  	  vearth[0] = linearVelocity(x_samples, vx_filter_samples, dt, delta);
-  	  vearth[1] = linearVelocity(y_samples, vy_filter_samples, dt, delta);
-  	  vearth[2] = linearVelocity(z_samples, vz_filter_samples, dt, delta);
+    // Convert IMU euler angles to quaternion
+    Quaterniond q_imu = euler2quatern(eu);
+    q_imu.normalize();
 
-  	  vx_filter_samples[0] = vx_filter_samples[1];
-  	  vx_filter_samples[1] = vx_filter_samples[2];
-  	  vx_filter_samples[2] = vx_filter_samples[3];
-  	  vx_filter_samples[3] = vx_filter_samples[4];
-  	  vx_filter_samples[4] = vearth[0];
-  	  vy_filter_samples[0] = vy_filter_samples[1];
-  	  vy_filter_samples[1] = vy_filter_samples[2];
-  	  vy_filter_samples[2] = vy_filter_samples[3];
-  	  vy_filter_samples[3] = vy_filter_samples[4];
-  	  vy_filter_samples[4] = vearth[1];
-  	  vz_filter_samples[0] = vz_filter_samples[1];
-  	  vz_filter_samples[1] = vz_filter_samples[2];
-  	  vz_filter_samples[2] = vz_filter_samples[3];
-  	  vz_filter_samples[3] = vz_filter_samples[4];
-  	  vz_filter_samples[4] = vearth[2];
+    x0_sign[qw] = q_imu.w();
+    x0_sign[qx] = q_imu.x();
+    x0_sign[qy] = q_imu.y();
+    x0_sign[qz] = q_imu.z();
 
-  	  return vearth;
+    Vector3d vi_mat;
+    vi_mat = estimateWordLinearVelocities(dt,t0);
+
+    Vector3d vb_mat;
+    vb_mat = rotateLinearVeloE2B(&q_imu,vi_mat);
+
+    // overwriting linear velocities in the body frame in state vector
+    x0_sign[vbx] = vb_mat[0];
+    x0_sign[vby] = vb_mat[1];
+    x0_sign[vbz] = vb_mat[2];
+
+    // Storing body angular velocities in state vector
+    x0_sign[wx] = actual_wx;
+    x0_sign[wy] = actual_wy;
+    x0_sign[wz] = actual_wz;
+
+
+    // Up to this point we already stored the 13 states required for the NMPC. So advertise them!
+    geometry_msgs::Quaternion cf_st_quat; // publisher for quaternion
+    geometry_msgs::Vector3    cf_st_lvb;  // publisher for the linear velocities w.r.t. the body frame
+    geometry_msgs::Vector3    cf_st_avb;  // publisher for the angular velocities w.r.t. the body frame
+
+
+    cf_st_quat.w = x0_sign[qw];
+    cf_st_quat.x = x0_sign[qx];
+    cf_st_quat.y = x0_sign[qy];
+    cf_st_quat.z = x0_sign[qz];
+    cf_st_lvb.x  = x0_sign[vbx];
+    cf_st_lvb.y  = x0_sign[vby];
+    cf_st_lvb.z  = x0_sign[vbz];
+    cf_st_avb.x  = x0_sign[wx];
+    cf_st_avb.y  = x0_sign[wy];
+    cf_st_avb.z  = x0_sign[wz];
+
+    m_quat_pub.publish(cf_st_quat);
+    m_lvb_pub.publish(cf_st_lvb);
+    m_avb_pub.publish(cf_st_avb);
+
     }
-
-    Vector3d rotateLinearVeloE2B(Quaterniond* q, Vector3d v_inertial){
-
-     	 Matrix3d Sq;
-     	 Vector3d vb;
-
-    	 double S11 = 2*(q->w()*q->w()+q->x()*q->x())-1;
-    	 double S12 = 2*(q->x()*q->y()+q->w()*q->z());
-    	 double S13 = 2*(q->x()*q->z()-q->w()*q->y());
-    	 double S21 = 2*(q->x()*q->y()-q->w()*q->z());
-    	 double S22 = 2*(q->w()*q->w()+q->y()*q->y())-1;
-    	 double S23 = 2*(q->y()*q->z()+q->w()*q->x());
-    	 double S31 = 2*(q->x()*q->z()+q->w()*q->y());
-    	 double S32 = 2*(q->y()*q->z()-q->w()*q->x());
-    	 double S33 = 2*(q->w()*q->w()+q->z()*q->z())-1;
-
-
-    	 Sq << S11,S12,S13,
-    	       S21,S22,S23,
-    	       S31,S32,S33;
-
-    	 vb = Sq*v_inertial;
-
-    	 return vb;
-    }
-
-    void iteration(const ros::TimerEvent& e){
-
-      if(e.last_real.isZero()) {
-	       t0 = e.current_real.toSec();
-      }
-
-      double dt = e.current_real.toSec() - e.last_real.toSec();
-
-      // Storing inertial positions in state vector
-      x0_sign[xq] = actual_x;
-      x0_sign[yq] = actual_y;
-      x0_sign[zq] = actual_z;
-
-      // Get the euler angles from the onboard stabilizer
-      euler eu;
-      eu.phi   = deg2Rad(actual_roll);
-      eu.theta = deg2Rad(actual_pitch);
-      eu.psi   = deg2Rad(actual_yaw);
-
-      // Convert IMU euler angles to quaternion
-      Quaterniond q_imu = euler2quatern(eu);
-      q_imu.normalize();
-
-      x0_sign[qw] = q_imu.w();
-      x0_sign[qx] = q_imu.x();
-      x0_sign[qy] = q_imu.y();
-      x0_sign[qz] = q_imu.z();
-
-      Vector3d vi_mat;
-      vi_mat = estimateWordLinearVelocities(dt,t0);
-
-      Vector3d vb_mat;
-      vb_mat = rotateLinearVeloE2B(&q_imu,vi_mat);
-
-      // overwriting linear velocities in the body frame in state vector
-      x0_sign[vbx] = vb_mat[0];
-      x0_sign[vby] = vb_mat[1];
-      x0_sign[vbz] = vb_mat[2];
-
-      // Storing body angular velocities in state vector
-      x0_sign[wx] = actual_wx;
-      x0_sign[wy] = actual_wy;
-      x0_sign[wz] = actual_wz;
-
-
-      // Up to this point we already stored the 13 states required for the NMPC. So advertise them!
-      geometry_msgs::Quaternion cf_st_quat; // publisher for quaternion
-      geometry_msgs::Vector3    cf_st_lvb;  // publisher for the linear velocities w.r.t. the body frame
-      geometry_msgs::Vector3    cf_st_avb;  // publisher for the angular velocities w.r.t. the body frame
-
-
-      cf_st_quat.w = x0_sign[qw];
-      cf_st_quat.x = x0_sign[qx];
-      cf_st_quat.y = x0_sign[qy];
-      cf_st_quat.z = x0_sign[qz];
-      cf_st_lvb.x  = x0_sign[vbx];
-      cf_st_lvb.y  = x0_sign[vby];
-      cf_st_lvb.z  = x0_sign[vbz];
-      cf_st_avb.x  = x0_sign[wx];
-      cf_st_avb.y  = x0_sign[wy];
-      cf_st_avb.z  = x0_sign[wz];
-
-      m_quat_pub.publish(cf_st_quat);
-      m_lvb_pub.publish(cf_st_lvb);
-      m_avb_pub.publish(cf_st_avb);
-
-      }
-    }
-
-private:
-
-    ros::Subscriber m_imu_sub;
-    ros::Subscriber m_euler_sub;
-    ros::Subscriber m_eRaptor_sub;
-    ros::Subscriber m_motors;
-
-    ros::Publisher m_quat_pub;
-    ros::Publisher m_lvb_pub;
-    ros::Publisher m_avb_pub;
-
-    // Variables for reading the IMU data
-    double actual_wx,actual_wy,actual_wz;
-    double actual_roll,actual_pitch,actual_yaw;
-    int32_t actual_m1,actual_m2,actual_m3,actual_m4;
-
-    // Variables for the mocap callback
-    double actual_x,actual_y,actual_z;
-
-    double vx,vy,vz;
-    std::vector<double> x_samples;
-    std::vector<double> y_samples;
-    std::vector<double> z_samples;
-    std::vector<double> vx_filter_samples;
-    std::vector<double> vy_filter_samples;
-    std::vector<double> vz_filter_samples;
-    double t0;
-
-    // Variables of the nmpc control process
-    double x0_sign[NX];
-    double yref_sign[(NY*N)+NY];
-
 };
 
 int main(int argc, char **argv)
@@ -383,7 +402,7 @@ int main(int argc, char **argv)
 
   ros::NodeHandle n("~");
   double frequency;
-  n.param("frequency", frequency, 50.0);
+  n.param("frequency", frequency, 66.6);
 
   meas_vec meas_vec(n);
   meas_vec.run(frequency);
