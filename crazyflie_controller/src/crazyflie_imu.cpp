f53e52c31d48abe0131293ade7c90965afd8a432
bbarroscarlos <barros@diag.uniroma1.it>
2020-01-05 18:41:05 +0100

    readme update

--- crazyflie_controller/src/crazyflie_imu.cpp
+++ crazyflie_controller/src/crazyflie_imu.cpp
@@ -1,3 +1,30 @@
+/*
+* Copyright 2020 Barbara Barros Carlos, Tommaso Sartor
+*
+* This file is part of crazyflie_nmpc.
+*
+* The MIT License (MIT)
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
 
@@ -12,35 +39,32 @@ using std::fixed;
 using std::showpos;
 
 
-
 class NMPC
 {
 public:
 
     NMPC(const ros::NodeHandle& n){
 
-        ros::NodeHandle nh;
-	m_imu_sub 	 = nh.subscribe("/crazyflie/imu", 1000, &NMPC::imuCallback, this);
+       ros::NodeHandle nh;
+	     m_imu_sub 	 = nh.subscribe("/crazyflie/imu", 1000, &NMPC::imuCallback, this);
 
     }
 
 private:
 
-
-
     void imuCallback(const sensor_msgs::Imu::ConstPtr& msg){
 
-	p  = msg->angular_velocity.x;
-	q  = msg->angular_velocity.y;
-	r  = msg->angular_velocity.z;
+    	p  = msg->angular_velocity.x;
+    	q  = msg->angular_velocity.y;
+    	r  = msg->angular_velocity.z;
 
-	abx = msg->linear_acceleration.x;
-	aby = msg->linear_acceleration.y;
-	abz = msg->linear_acceleration.z;
+    	abx = msg->linear_acceleration.x;
+    	aby = msg->linear_acceleration.y;
+    	abz = msg->linear_acceleration.z;
 
-	ROS_INFO_STREAM(fixed << showpos << "\nQuad flight data at time [" << msg->header.stamp << "s "<< "]" << endl
-		        << "Gyro [p,q,r] = [" << p << ", " << q << ", " << r << "]" << endl
-		        << "Acc [abx,aby,abz] = [" << abx << ", " << aby << ", " << abz << "]" << endl);
+    	ROS_INFO_STREAM(fixed << showpos << "\nQuad flight data at time [" << msg->header.stamp << "s "<< "]" << endl
+    		        << "Gyro [p,q,r] = [" << p << ", " << q << ", " << r << "]" << endl
+    		        << "Acc [abx,aby,abz] = [" << abx << ", " << aby << ", " << abz << "]" << endl);
     }
 
 private:
@@ -48,7 +72,6 @@ private:
     ros::Subscriber m_imu_sub;
 
     // Variables for reading the IMU data
-    float q1,q2,q3,q4;
     float p;
     float q;
     float r;
