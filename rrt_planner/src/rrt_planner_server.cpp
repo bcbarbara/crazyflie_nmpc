37c61b6621c2d7e40539bfdecea4033b6cc1729f
Devansh Dhrafani <dhrafani98@gmail.com>
2023-12-10 20:50:11 -0500

    Specify map filename in rrt planner service call

--- rrt_planner/src/rrt_planner_server.cpp
+++ rrt_planner/src/rrt_planner_server.cpp
@@ -7,6 +7,7 @@
 #include "ros/ros.h"
 
 Map* env_map;
+std::string maps_package_path;
 std::string map_path;
 int numOfDOFs;
 double epsilon;
@@ -14,6 +15,16 @@ double sample_resolution;
 
 bool rrt_planner_callback(rrt_planner::GetRRTPlan::Request  &req, rrt_planner::GetRRTPlan::Response &res)
 {
+
+    // If map_filename is specified, then load the map again
+    if (req.map_filename.data != ""){
+        // Load map
+        std::string map_path = maps_package_path + "/config/" + req.map_filename.data;
+        Map temp_map = Map(map_path);
+        env_map = &temp_map;
+        ROS_INFO("Server called with new map file. Loaded map from %s", map_path.c_str());
+    }
+
     // Start and goal points
     double* start_point = (double*)malloc(numOfDOFs*sizeof(double));
     double* goal_point = (double*)malloc(numOfDOFs*sizeof(double));
@@ -105,7 +116,7 @@ int main(int argc, char **argv)
     sample_resolution = pnh.param<double>("sample_resolution", 0.1);
 
     // find map_path from maps package
-    std::string maps_package_path = ros::package::getPath("maps");
+    maps_package_path = ros::package::getPath("maps");
     map_path = maps_package_path + "/config/map.yaml";
 
     // Load map
