aa0f486cd2eb08d9e73edb27010642a2268ce28b
AneeshSinha1001 <aneeshsinha01@gmail.com>
2023-12-10 22:33:23 -0500

    Updated map gen and bought mocap lanuch files locally

--- crazyflie_controller/src/map_gen.py
+++ crazyflie_controller/src/map_gen.py
@@ -5,50 +5,88 @@ import yaml
 from geometry_msgs.msg import PoseStamped
 from collections import OrderedDict
 from std_srvs.srv import Empty, EmptyResponse
-
+from std_srvs.srv import Trigger, TriggerResponse
+import os
 class MapGenerator:
     def __init__(self):
-        self.obstacles = ['sdsd']
+        obstacle = {
+            'id': -1,
+            'x': 0.0,
+            'y': 0.0,
+            'r': 0.05  # radius, adjust as needed
+        }
+        self.obstacles = [obstacle, obstacle]
+        self.num_obstacles = 0
         rospy.init_node('generate_map_server', anonymous=True)
-        rospy.Subscriber('/mocap/object_pos_1', PoseStamped, self.pose_callback)
-        self.service = rospy.Service('generate_map', Empty, self.handle_generate_map)
+        rospy.Subscriber('/mocap/object_pos_1', PoseStamped, self.pose_callback_1)
+        rospy.Subscriber('/mocap/object_pos_2', PoseStamped, self.pose_callback_2)
+        self.service = rospy.Service('generate_map', Trigger, self.handle_generate_map)
         
-        # rospy.Subscriber('/mocap/object_pos_2', PoseStamped, self.pose_callback)
-        # rospy.Subscriber('/mocap/object_pos_3', PoseStamped, self.pose_callback)
-    
+
     def handle_generate_map(self, request):
-        if len(self.obstacles) >= 1:  # number of obstacles, adjust as needed
-            self.gen_map() 
-        return EmptyResponse()
-    def pose_callback(self, data):
+        if len(self.obstacles) >= 2:  # number of obstacles, adjust as needed
+            filename=self.gen_map()
+            return TriggerResponse(success=True, message=filename) 
+        else:
+            return TriggerResponse(success=False, message="Not enough obstacles")
+       
+    
+    def pose_callback_1(self, data):
 
         obstacle = {
             'id': 0,
             'x': data.pose.position.x,
             'y': data.pose.position.z,
-            'r': 0.15  # radius, adjust as needed
+            'r': 0.05  # radius, adjust as needed
         }
         if self.is_valid_pose(obstacle):
             self.obstacles[0]=obstacle
-        
-        
+    
+    def pose_callback_2(self, data):
 
+        obstacle = {
+            'id': 1,
+            'x': data.pose.position.x,
+            'y': data.pose.position.z,
+            'r': 0.05  # radius, adjust as needed
+        }
+        if self.is_valid_pose(obstacle):
+            self.obstacles[1]=obstacle
+        
+        
     def is_valid_pose(self, obstacle):
-        return obstacle['x'] >= -5.0 and obstacle['x'] <= 10.0 and obstacle['y'] >= -5.0 and obstacle['y'] <= 10.0
+        return obstacle['x'] >= -0.5 and obstacle['x'] <= 1.5 and obstacle['y'] >= -1.8 and obstacle['y'] <= 0.5
     
     def gen_map(self):
+        for i in range(len(self.obstacles)):
+            if(self.obstacles[i]['id']!=-1):
+                self.num_obstacles+=1
+
         map_data = {
-        'num_obstacles': len(self.obstacles),
+        'num_obstacles': self.num_obstacles,
         'x_min': -0.5,
         'x_max': 1.5,
         'y_min': -1.8,
         'y_max': 0.5,
-        'obstacles': {obstacle['id']: {key: val for key, val in obstacle.items() if key != 'id'} for obstacle in self.obstacles}
+        'obstacles': {obstacle['id']: {key: val for key, val in obstacle.items() if key != 'id'} for obstacle in self.obstacles if obstacle['id'] != -1}
     }
-        filename='/home/aneesh/acsi_ws/src/acsi_crazyflie_nmpc/maps/config/map.yaml'
+        if os.path.exists('/home/aneesh/acsi_ws/src/acsi_crazyflie_nmpc/maps/config/counter.txt'):
+            with open('/home/aneesh/acsi_ws/src/acsi_crazyflie_nmpc/maps/config/counter.txt', 'r') as f:
+                counter = int(f.read())
+        else:
+            counter = 0
+
+        counter += 1
+        filename = f'/home/aneesh/acsi_ws/src/acsi_crazyflie_nmpc/maps/config/map{counter}.yaml'
+
+        # filename='/home/aneesh/acsi_ws/src/acsi_crazyflie_nmpc/maps/config/map.yaml'
         with open(filename, 'w') as f:
             yaml.dump(map_data, f, sort_keys=False)
-        # rospy.signal_shutdown('Map generated')
+
+        with open('/home/aneesh/acsi_ws/src/acsi_crazyflie_nmpc/maps/config/counter.txt', 'w') as f:
+            f.write(str(counter))
+        
+        return f"map{counter}.yaml"
 
 if __name__ == '__main__':
     MapGenerator()
