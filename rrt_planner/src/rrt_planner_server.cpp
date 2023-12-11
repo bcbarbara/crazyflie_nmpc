7bcd3d2ad364b493d8ae7baa3b78dc9ccde0ed07
Devansh Dhrafani <dhrafani98@gmail.com>
2023-12-10 20:31:08 -0500

    Find map_path from maps package

--- rrt_planner/src/rrt_planner_server.cpp
+++ rrt_planner/src/rrt_planner_server.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <ros/package.h>
 #include <nav_msgs/Path.h>
 #include <rrt_planner/rrt_planner.h>
 #include <rrt_planner/GetRRTPlan.h>
@@ -98,10 +99,14 @@ int main(int argc, char **argv)
     // ros::ServiceClient client = nh.serviceClient<std_srvs::Empty>("generate_map");
     // std_srvs::Empty srv;
     std::string map_path;
-    pnh.param<std::string>("/rrt_planner_server/map_path", map_path, "/home/aneesh/acsi_ws/src/acsi_crazyflie_nmpc/maps/config/map.yaml");
+    // pnh.param<std::string>("/rrt_planner_server/map_path", map_path, "/home/devanshdhrafani/acsi_ws/src/acsi_crazyflie_nmpc/maps/config/map.yaml");
     numOfDOFs = pnh.param<int>("numOfDOFs", 2);
     epsilon = pnh.param<double>("epsilon", 1.0);
     sample_resolution = pnh.param<double>("sample_resolution", 0.1);
+
+    // find map_path from maps package
+    std::string maps_package_path = ros::package::getPath("maps");
+    map_path = maps_package_path + "/config/map.yaml";
 
     // Load map
     Map temp_map = Map(map_path);
